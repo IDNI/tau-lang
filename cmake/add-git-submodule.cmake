0dd4981ec8d9953a01bfb6db4ac7095e9e756d3f
castrod <david.castroesteban@gmail.com>
2023-04-28 12:16:18 +0200

    fixing error with parser submodule

--- cmake/add-git-submodule.cmake
+++ cmake/add-git-submodule.cmake
@@ -1,14 +1,16 @@
 cmake_minimum_required(VERSION 3.22.1 FATAL_ERROR)
 
 function(add_git_submodule dir)
-find_package(Git REQUIRED)
+  find_package(Git REQUIRED)
 
-if(NOT EXISTS ${dir}/CMakeLists.txt)
-  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMAND_ERROR_IS_FATAL ANY)
-endif()
-
+  if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git" AND NOT EXISTS "${dir}/CMakeLists.txt")
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- "${dir}"
+      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+      RESULT_VARIABLE GIT_SUBMOD_RESULT)
+      if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+        message(FATAL_ERROR "git submodule update --init --recursive failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+      endif()
+  endif()
 add_subdirectory(${dir})
 
 endfunction(add_git_submodule)
