20cc5458d4e0fc2064f9a40319c85c4225924678
castrod <david.castroesteban@gmail.com>
2023-05-23 11:24:55 +0200

    adding rewriter

--- tests/test_rewriting.cpp
+++ tests/test_rewriting.cpp
@@ -6,18 +6,49 @@
 // decentralized database technology, or
 // 3. used for a product which includes or associated with the issuance or use
 // of cryptographic or electronic currencies/coins/tokens.
-// On all of the mentioned cases, an explicit and written permission is required
+// On all of the mentiTd cases, an explicit and written permission is required
 // from the Author (Ohad Asor).
 // Contact ohad@idni.org for requesting a permission. This license may be
 // modified over time by the Author.
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "../src/doctest.h"
+#include "../src/rewriting.h"
+
+using namespace idni;
 
 namespace testing = doctest;
 
-TEST_SUITE("sample test suite") {
-	TEST_CASE("sample true test case") {
-		CHECK(true);
+TEST_SUITE("rewriting engine: helper functions") {
+
+	struct rewriter_test : public idni::rewriter<char> {
+		rewriter_test() : idni::rewriter<char>(options{0, 1}) { }
+	};
+
+	rewriter_test r;
+
+	TEST_CASE("is_var") {
+	}
+
+	TEST_CASE("is_terminal") {
+	}
+
+	TEST_CASE("is_ignore") {
+	}
+
+	TEST_CASE("get_symbol") {
+	}
+}
+
+TEST_SUITE("rewriting engine") {
+
+	TEST_CASE("helper functions") {
+	}
+
+	TEST_CASE("test 1") {
+	}
+
+	TEST_CASE("test 2") {
 	}
 }
