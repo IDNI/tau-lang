41474a314e9655ee1db35a8d09b194705339e109
castrod <david.castroesteban@gmail.com>
2023-08-03 07:45:58 +0200

    adding extra test files

---
+++ tests/test_normalizer2.cpp
@@ -0,0 +1,33 @@
+// LICENSE
+// This software is free for use and redistribution while including this
+// license notice, unless:
+// 1. is used for commercial or non-personal purposes, or
+// 2. used for a product which includes or associated with a blockchain or other
+// decentralized database technology, or
+// 3. used for a product which includes or associated with the issuance or use
+// of cryptographic or electronic currencies/coins/tokens.
+// On all of the mentiTd cases, an explicit and written permission is required
+// from the Author (Ohad Asor).
+// Contact ohad@idni.org for requesting a permission. This license may be
+// modified over time by the Author.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "../src/doctest.h"
+#include "../src/normalizer2.h"
+#include "../src/bdd_handle.h"
+
+using namespace idni::rewriter;
+using namespace idni::tau;
+using namespace std;
+
+namespace testing = doctest;
+
+TEST_SUITE("normalizer2") {
+
+	TEST_CASE("sample true test case") {
+		CHECK(true);
+	}
+
+}
+
