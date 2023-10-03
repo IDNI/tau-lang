7b860faceebff06ac835d10d000fd0d794d55131
castrod <david.castroesteban@gmail.com>
2023-10-03 13:30:08 +0200

    addint dummy tests for bindings & type resolution

---
+++ tests/test_type_system.cpp
@@ -0,0 +1,34 @@
+// LICENSE
+// This software is free for use and redistribution while including this
+// license notice, unless:
+// 1. is used for commercial or non-personal purposes, or
+// 2. used for a product which includes or associated with a blockchain or other
+// decentralized database technology, or
+// 3. used for a product which includes or associated with the issuance or use
+// of cryptographic or electronic currencies/coins/tokens.
+// On all of the mentioned cases, an explicit and written permission is required
+// from the Author (Ohad Asor).
+// Contact ohad@idni.org for requesting a permission. This license may be
+// modified over time by the Author.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "../src/doctest.h"
+#include "../src/defs.h"
+#include "../src/formula.h"
+#include "../src/bool.h"
+#include "test_helpers.h"
+
+using namespace idni::rewriter;
+using namespace idni::tau;
+using namespace std;
+
+namespace testing = doctest;
+
+TEST_SUITE("type system") {
+
+	TEST_CASE("dummy") {
+		CHECK( false );
+	}
+
+}
