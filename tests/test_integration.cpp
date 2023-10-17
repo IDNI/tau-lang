0ac4b7cea7d119c2d79c36a1858da7773e7855dc
castrod <david.castroesteban@gmail.com>
2023-10-17 12:50:38 +0200

    adding infra for Ohad's integration tests

---
+++ tests/test_integration.cpp
@@ -0,0 +1,37 @@
+// LICENSE
+// This software is free for use and redistribution while including this
+// license notice, unless:
+// 1. is used for commercial or non-personal purposes, or
+// 2. used for a product which includes or associated with a blockchain or other
+// decentralized database technology, or
+// 3. used for a product which includes or associated with the issuance or use
+// of cryptographic or electronic currencies/coins/tokens.
+// On all of the mentiTd cases, an explicit and written permission is required
+// from the Author (Ohad Asor).
+// Contact ohad@idni.org for requesting a permission. This license may be
+// modified over time by the Author.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <cassert>
+
+#include "../src/doctest.h"
+#include "../src/formula.h"
+#include "../src/bool.h"
+#include "../src/bdd_handle.h"
+#include "../src/normalizer2.h"
+#include "test_helpers.h"
+
+using namespace idni::rewriter;
+using namespace idni::tau;
+
+namespace testing = doctest;
+
+// TDDO (HIGH) add all Ohad's test formulas
+
+TEST_SUITE("Ohad's test formulas") {
+
+	TEST_CASE("formula 1") {
+		CHECK( false );
+	} 
+}
