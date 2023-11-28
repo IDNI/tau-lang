674207c8bb9f3305d4f8228b0458f84cb1a4a4dc
castrod <david.castroesteban@gmail.com>
2023-11-28 11:21:46 +0100

    fully implementing debug_tau_normalizer (pc)

--- tests/debug_tau_normalizer.cpp
+++ tests/debug_tau_normalizer.cpp
@@ -12,6 +12,8 @@
 // modified over time by the Author.
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "../src/defs.h"
 #include "../src/bdd_handle.h"
@@ -25,8 +27,22 @@ using namespace idni::tau;
 
 int main(int argc, char** argv) {
 	if (argc != 2) return cerr << argv[0] << ": requires 1 argument: formula file\n", 1;
+
 	string formula_file(argv[1]);
-	// TODO (HIGH) read formula from file, normalize it and print the output
+	ifstream file(formula_file);
+	string content, line;
+
+	while(!file.eof())
+		while(std::getline(file, line))
+			content += line + "\n";
+	file.close();
+
+	bdd_test_factory bf;
+	factory_binder<bdd_test_factory, bdd_test> fb(bf);
+	auto formula = make_formula_using_factory<factory_binder<bdd_test_factory_t, bdd_test>, bdd_test>(content, fb); 
+
+	normalizer<bdd_test>(formula);
+
 	return 0;
 }
 
