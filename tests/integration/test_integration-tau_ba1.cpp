42fff8c9c1c027f87a81a7784ef0ce447368db49
Tomáš Klapka <tomas@idni.org>
2024-02-22 13:45:09 +0100

    draft tau_ba tests for operators with normalization

--- tests/integration/test_integration-tau_ba1.cpp
+++ tests/integration/test_integration-tau_ba1.cpp
@@ -89,3 +89,40 @@ TEST_SUITE("formulas: two level rec, no variables, no bindings and no quantifier
 		CHECK( check.has_value() );
 	}
 }
+
+tau_ba<bdd_test> get_tau_ba(const char* src) {
+	bdd_test_factory bf;
+	tau_factory<bdd_test_factory, bdd_test> fb(bf);
+	factory_binder<tau_factory<bdd_test_factory, bdd_test>, tau_ba<bdd_test>, bdd_test> fbinder(fb);
+	rr<nso<tau_ba<bdd_test>, bdd_test>> nso_rr = make_nso_rr_using_factory<
+			factory_binder<tau_factory<bdd_test_factory, bdd_test>,tau_ba<bdd_test>, bdd_test>,
+			tau_ba<bdd_test>, bdd_test>(src, fbinder);
+	auto value = nso_rr.main
+		| tau_parser::bf_eq
+		| tau_parser::bf
+		| tau_parser::bf_constant
+		| tau_parser::constant
+		| only_child_extractor<tau_ba<bdd_test>, bdd_test>;
+	return get<tau_ba<bdd_test>>(get<variant<
+			tau_ba<bdd_test>, bdd_test>>(value.value()->value));
+}
+
+tau_ba<bdd_test> normal(const tau_ba<bdd_test>& f) {
+	auto normalized = normalizer<tau_ba<bdd_test>, bdd_test>(f.rr_nso);
+	return normalized;
+}
+
+TEST_SUITE("tau_ba") {
+	TEST_CASE("tau_ba operators") {
+		auto T = get_tau_ba("( { : ({ : F.} = 0). } = 0 ).");
+		auto F = get_tau_ba("( { : ({ : T.} = 0). } = 0 ).");
+		auto TorF  = T | F;
+		auto TandF = T & F;
+		cout << "T: "     << T     << " \n\tnormal: " << normal(T)     << "\n";
+		cout << "F: "     << F     << " \n\tnormal: " << normal(F)     << "\n";
+		cout << "T | F: " << TorF  << " \n\tnormal: " << normal(TorF)  << "\n";
+		cout << "T & F: " << TandF << " \n\tnormal: " << normal(TandF) << "\n";
+
+		CHECK( true );
+	}
+}
