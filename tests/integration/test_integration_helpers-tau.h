878668283e1e12e39e22338ba0e137ae167b6977
castrod <david.castroesteban@gmail.com>
2024-02-26 08:26:25 +0100

    adding explicit tests for tau_ba operators

--- tests/integration/test_integration_helpers-tau.h
+++ tests/integration/test_integration_helpers-tau.h
@@ -59,4 +59,26 @@ rr<nso<tau_ba<bdd_test>, bdd_test>> normalize_test_tau(const char* src) {
 	return normalizer<tau_ba<bdd_test>, bdd_test>(nso_rr);
 }
 
+tau_ba<bdd_test> get_tau_ba(const char* src) {
+	bdd_test_factory bf;
+	tau_factory<bdd_test_factory, bdd_test> fb(bf);
+	factory_binder<tau_factory<bdd_test_factory, bdd_test>, tau_ba<bdd_test>, bdd_test> fbinder(fb);
+	rr<nso<tau_ba<bdd_test>, bdd_test>> nso_rr = make_nso_rr_using_factory<
+			factory_binder<tau_factory<bdd_test_factory, bdd_test>,tau_ba<bdd_test>, bdd_test>,
+			tau_ba<bdd_test>, bdd_test>(src, fbinder);
+	auto value = nso_rr.main
+		| tau_parser::bf_eq
+		| tau_parser::bf
+		| tau_parser::bf_constant
+		| tau_parser::constant
+		| only_child_extractor<tau_ba<bdd_test>, bdd_test>;
+	return get<tau_ba<bdd_test>>(get<variant<
+			tau_ba<bdd_test>, bdd_test>>(value.value()->value));
+}
+
+tau_ba<bdd_test> normal(const tau_ba<bdd_test>& f) {
+	auto normalized = normalizer<tau_ba<bdd_test>, bdd_test>(f.nso_rr);
+	return normalized;
+}
+
 #endif // __TEST_INTEGRATION_HELPERS_TAU_H__
