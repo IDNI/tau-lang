c832f28d4e1461393b1a413f3409a375557b9c89
castrod <david.castroesteban@gmail.com>
2024-02-27 15:20:07 +0100

    speccing out executor overall execution

--- src/executor.h
+++ src/executor.h
@@ -11,9 +11,68 @@
 // Contact ohad@idni.org for requesting a permission. This license may be
 // modified over time by the Author.
 
-#ifdef __EXECUTOR_H__
+#ifndef __EXECUTOR_H__
 #define __EXECUTOR_H__
 
+#include "tau.h"
+#include "satisfiability.h"
 
+template<typename...BAs>
+using inputs = std::map<gssotc<BAs...>, gssotc<BAs...>>;
+
+template<typename...BAs>
+using outputs = std::map<gssotc<BAs...>, gssotc<BAs...>>;
+
+
+template <typename...BAs>
+struct compiled_gssotc_clause {
+
+	gssotc<BAs...> clause;
+	gssotc<BAs...> compiled;
+};
+
+template <typename...BAs>
+struct compiled_gssotc {
+
+	std::vector<compiled_gssotc_clause<BAs...>> compiled_clauses;
+};
+
+template <typename...BAs>
+struct executor {
+
+	gssotc<BAs...> execute(const gssotc<BAs...>& input) {
+			// TODO (HIGH) implement
+			return input;
+	}
+
+	compiled_gssotc<BAs...> compiled;
+	inputs<BAs...> ins;
+	outputs<BAs...> outs;
+};
+
+template <typename...BAs>
+compiled_gssotc_clause<BAs...> compile_gssotc_clause(const gssotc<BAs...>& clause) {
+	// TODO (HIGH) implement
+	auto collapsed = clause |
+		repeat_all<step<tau_ba<BAs...>, BAs...>, tau_ba<BAs...>, BAs...>(
+			simplify_tau<tau_ba<BAs...>, BAs...>
+			| collapse_positives_tau<tau_ba<BAs...>, BAs...>);
+
+	return compiled_gssotc_clause<BAs...>{};
+}
+
+template <typename...BAs>
+compiled_gssotc<BAs...> compile(const gssotc<BAs...>& form) {
+	auto dnf = form
+		| repeat_all<step<tau_ba<BAs...>, BAs...>, tau_ba<BAs...>, BAs...>(
+			to_dnf_tau<tau_ba<BAs...>, BAs...>
+			| simplify_tau<tau_ba<BAs...>, BAs...>);
+	auto clauses = get_gssotc_clauses(dnf);
+	for (auto& clause: clauses) {
+		compile_clause(clause);
+	}
+
+	return compiled_gssotc<BAs...>{};
+}
 
 # endif //__EXECUTOR_H__
