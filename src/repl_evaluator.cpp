4775951cde6126664a25a56e23a9892ce4226919
Tomáš Klapka <tomas@idni.org>
2024-03-02 00:38:29 +0100

    repl supports normalize nso_rr and wff

--- src/repl_evaluator.cpp
+++ src/repl_evaluator.cpp
@@ -14,6 +14,7 @@
 #include "repl_evaluator.h"
 #include "parser_instance.h"
 #include "normalizer2.h"
+#include "nso_rr.h"
 #include "bdd_binding.h"
 
 namespace idni {
@@ -21,12 +22,10 @@ namespace idni {
 using namespace idni::tau;
 using namespace idni::rewriter;
 
-static void version() {
-	cout << "\nTau version: " << GIT_DESCRIBED << "\n";
-}
+void version() { cout << "\nTau version: " << GIT_DESCRIBED << "\n"; }
 
 // TODO (LOW) write proper help messages
-static void help(size_t nt = tau_parser::help_sym) {
+void help(size_t nt = tau_parser::help_sym) {
 	cout << "\n";
 	switch (nt) {
 	case tau_parser::help_sym: cout
@@ -62,17 +61,36 @@ static void help(size_t nt = tau_parser::help_sym) {
 	}
 }
 
-void normalize_nso_rr(const sp_tau_source_node& /*n*/)
-{
-	// TODO (HIGH) implement nso_rr normalization
+void normalize_nso_rr(sp_tau_source_node src) {
+	bdd_factory bf;
+	tau_factory<bdd_factory, bdd_binding> fb(bf);
+	factory_binder<decltype(fb),
+		tau_ba<bdd_binding>, bdd_binding> fbinder(fb);
+	auto nso_rr = make_nso_rr_using_factory<decltype(fbinder),
+			tau_ba<bdd_binding>, bdd_binding>(src, fbinder);
+	auto n = normalizer<tau_ba<bdd_binding>, bdd_binding>(nso_rr);
+	cout << n << "\n";
 }
 
-void normalize_wff(const sp_tau_source_node& /*n*/)
-{
-	// TODO (HIGH) implement wff normalization
+void normalize_wff(sp_tau_source_node src) {
+	auto form = make_tau_code<tau_ba<bdd_binding>, bdd_binding>(src)
+		 | tau_parser::wff
+		 | optional_value_extractor<sp_bdd_node>;
+	bdd_factory bf;
+	tau_factory<bdd_factory, bdd_binding> fb(bf);
+	factory_binder<decltype(fb),
+		tau_ba<bdd_binding>, bdd_binding> fbinder(fb);
+	bind_transformer<decltype(fbinder),
+		tau_ba<bdd_binding>, bdd_binding> bs(fbinder);
+	auto nso = post_order_traverser<
+			decltype(bs),
+			all_t<sp_bdd_node>,
+			sp_bdd_node>(bs, all<sp_bdd_node>)(form);
+	auto n = normalizer<tau_ba<bdd_binding>, bdd_binding>(nso);
+	cout << n << "\n";
 }
 
-const sp_tau_source_node extract_tau_source(tau_parser::forest_type* f,
+sp_tau_source_node extract_tau_source(tau_parser::forest_type* f,
 	const tau_parser::node_type& n)
 {
 	return make_node_from_forest<
@@ -88,18 +106,8 @@ const sp_tau_source_node extract_tau_source(tau_parser::forest_type* f,
 			f, n);
 }
 
-bool is_cli_sym(size_t nt) {
-	static std::set<size_t> cli_symbols{
-		tau_parser::version_sym,
-		tau_parser::quit_sym,
-		tau_parser::output_sym,
-		tau_parser::selection_sym,
-		tau_parser::instantiate_sym,
-		tau_parser::substitute_sym,
-		tau_parser::normalize_sym,
-		tau_parser::file_sym
-	};
-	return cli_symbols.find(nt) != cli_symbols.end();
+sp_tau_source_node extract_tau_source(tau_parser::forest_type* f) {
+	return extract_tau_source(f, f->root());
 }
 
 // traverses a tree and resolves repl commands. runs them with required
@@ -109,47 +117,44 @@ int traverse(tau_parser::forest_type* f) {
 	int quit = 0;
 	struct context { // to keep track of where we are in the tree
 		bool
-			h = false,         // normalize
-			n = false,         // help
-			q_nso_rr = false,
-			q_wff = false
+			h = false, // normalize
+			n = false  // help
 			;
 	} x;
 	auto cb_enter = [&x, &f, &quit] (const auto& n) {
 		if (!n.first.nt()) return; // skip if terminal
 		auto nt = n.first.n(); // get nonterminal id
-		// simple commands which do not require any extraction
-		if (nt == tau_parser::quit) cout << "\nQuit.", quit = 1;
+		if      (nt == tau_parser::quit)    cout << "\nQuit.", quit = 1;
 		else if (nt == tau_parser::version) version();
-		// tree nodes for which we need to know we are inside
-		// them are set to true in context
-		else if (nt == tau_parser::help) x.h = true;
-		else if (nt == tau_parser::normalize)
-			x.n = true;
-		else if (nt == tau_parser::q_nso_rr)
-			x.q_nso_rr = true;
-		else if (nt == tau_parser::q_wff)
-			x.q_wff = true;
+		// set flags for nodes we need to know we are in their subtree
+		else if (nt == tau_parser::help)      x.h = true;
+		else if (nt == tau_parser::normalize) x.n = true;
 	};
 	auto cb_exit = [&f, &x] (const auto& n, const auto&) {
 		if (!n.first.nt()) return; // skip if terminal
 		auto nt = n.first.n(); // get nonterminal id
-		if (x.n) {
-			if (nt == tau_parser::nso_rr && x.q_nso_rr) {
-				// if we finished traversing nso_rr
-				// and we are in normalization a q_nso_rr
-				x.n = x.q_nso_rr = false,
-				normalize_nso_rr(
-					extract_tau_source(f, n));
-			} else if (nt == tau_parser::wff && x.q_wff) {
-				// if we finished traversing wff
-				// and we are in normalization a q_wff
-				x.n = x.q_wff = false,
+		if (x.n) { // normalize
+			if (nt == tau_parser::q_nso_rr) {
+				x.n = false,
+				normalize_nso_rr(extract_tau_source(f, n));
+			} else if (nt == tau_parser::q_wff) {
+				x.n = false,
 				normalize_wff(extract_tau_source(f, n));
 			}
-		} else if (x.h) {
+		} else if (x.h) { // help
+			static const std::set<size_t> cli_symbols{
+				tau_parser::version_sym,
+				tau_parser::quit_sym,
+				tau_parser::output_sym,
+				tau_parser::selection_sym,
+				tau_parser::instantiate_sym,
+				tau_parser::substitute_sym,
+				tau_parser::normalize_sym,
+				tau_parser::file_sym
+			};
 			if (nt == tau_parser::help) help(), x.h = false;
-			else if (is_cli_sym(nt))  help(nt), x.h = false;
+			else if (cli_symbols.find(nt) != cli_symbols.end())
+				help(nt), x.h = false;
 		}
 	};
 	f->traverse(cb_enter, cb_exit);
@@ -189,17 +194,9 @@ int repl_evaluator::eval(const std::string& src) {
 	auto f = parser_instance<tau_parser>()
 		.parse(src.c_str(), src.size(), {
 			.start = tau_parser::cli });
-	DBG(check_parser_result<tau_parser>(src,f.get(),tau_parser::cli);)
-	// make tau source tree
-	auto n = make_node_from_forest<tau_parser,
-		rewriter::drop_location_t<tau_parser::node_type,
-			tau::tau_source_sym>,
-		tau_parser::node_type,
-		tau::tau_source_sym>(
-			drop_location<tau_parser::node_type,
-				tau::tau_source_sym>, f.get());
-	// debug the tree
-	DBG(print_sp_tau_source_node_tree(std::cerr<<"\n", n) << "\n";)
+	DBG(check_parser_result<tau_parser>(src, f.get(), tau_parser::cli);)
+	DBG(print_sp_tau_source_node_tree(std::cerr << "\n",
+		extract_tau_source(f.get())) << "\n";)
 	// run forest traversal to resolve commands
 	if (traverse(f.get())) return 1;
 	return 0;
