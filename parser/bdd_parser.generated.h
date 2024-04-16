c2510a212ce2c2a9a8cd642ada9af655532ffd3d
Tomáš Klapka <tomas@idni.org>
2024-04-17 01:16:51 +0200

    pretty printer pp()

--- parser/bdd_parser.generated.h
+++ parser/bdd_parser.generated.h
@@ -29,7 +29,7 @@ struct bdd_parser {
 			const std::vector<terminal_type>&)>;
 	bdd_parser() :
 		nts(load_nonterminals()), cc(load_cc()),
-		g(nts, load_prods(), nt(6), cc, load_grammar_opts()),
+		g(nts, load_prods(), nt(4), cc, load_grammar_opts()),
 		p(g, load_opts()) {}
 	std::unique_ptr<forest_type> parse(const char_type* data, size_t size,
 		parse_options po = {}) { return p.parse(data, size, po); }
@@ -41,32 +41,63 @@ struct bdd_parser {
 	std::unique_ptr<forest_type> parse(int fd, parse_options po = {})
 		{ return p.parse(fd, po); }
 #endif //WIN32
-	sptree_type parse_and_shape(forest_type* f) {
+	sptree_type shape(forest_type* f, const node_type& n) {
 		idni::tree_shaping_options opt;
 		opt.to_trim = g.opt.to_trim;
 		opt.to_trim_children = g.opt.to_trim_children;
 		opt.trim_terminals = g.opt.trim_terminals;
 		opt.to_inline = g.opt.to_inline;
 		opt.inline_char_classes = g.opt.inline_char_classes;
-		return f->get_shaped_tree(opt);
+		return f->get_shaped_tree(n, opt);
 	}
 	sptree_type parse_and_shape(const char_type* data, size_t size,
-		parse_options po = {}) {
-			return parse_and_shape(p.parse(data, size, po).get()); }
+		const node_type& n, parse_options po = {})
+	{
+		return shape(p.parse(data, size, po).get(), n);
+	}
+	sptree_type parse_and_shape(const char_type* data, size_t size,
+		parse_options po = {})
+	{
+		auto f = p.parse(data, size, po);
+		return shape(f.get(), f->root());
+	}
 	sptree_type parse_and_shape(std::basic_istream<char_type>& is,
-		parse_options po = {}) {
-				return parse_and_shape(p.parse(is, po).get()); }
+		const node_type& n, parse_options po = {})
+	{
+		return shape(p.parse(is, po).get(), n);
+	}
+	sptree_type parse_and_shape(std::basic_istream<char_type>& is,
+		parse_options po = {})
+	{
+		auto f = p.parse(is, po);
+		return shape(f.get(), f->root());
+	}
 	sptree_type parse_and_shape(const std::string& fn,
-		parse_options po = {}) {
-				return parse_and_shape(p.parse(fn, po).get()); }
+		const node_type& n, parse_options po = {})
+	{
+		return shape(p.parse(fn, po).get(), n);
+	}
+	sptree_type parse_and_shape(const std::string& fn,
+		parse_options po = {})
+	{
+		auto f = p.parse(fn, po);
+		return shape(f.get(), f->root());
+	}
 #ifndef WIN32
+	sptree_type parse_and_shape(int fd, const node_type& n, parse_options po = {})
+	{
+		return shape(p.parse(fd, po).get(), n);
+	}
 	sptree_type parse_and_shape(int fd, parse_options po = {})
-		{ return parse_and_shape(p.parse(fd, po).get()); }
+	{
+		auto f = p.parse(fd, po);
+		return shape(f.get(), f->root());
+	}
 #endif //WIN32
 	bool found(size_t start = SIZE_MAX) { return p.found(start); }
 	typename parser_type::error get_error() { return p.get_error(); }
 	enum nonterminal {
-		nul, space, alpha, alnum, _, __, start, bdd, __E___0, disjunction, 
+		nul, space, alpha, alnum, start, _, bdd, __, __E___0, disjunction, 
 		bdd2, conjunction, bdd3, exclusive_or, bdd4, negation, bdd5, lit, __E_exclusive_or_1, var, 
 		T, F, __E_var_2, __E_var_3, 
 	};
@@ -98,7 +129,7 @@ private:
 	idni::nonterminals<char_type, terminal_type> load_nonterminals() const {
 		idni::nonterminals<char_type, terminal_type> nts{};
 		for (const auto& nt : {
-			"", "space", "alpha", "alnum", "_", "__", "start", "bdd", "__E___0", "disjunction", 
+			"", "space", "alpha", "alnum", "start", "_", "bdd", "__", "__E___0", "disjunction", 
 			"bdd2", "conjunction", "bdd3", "exclusive_or", "bdd4", "negation", "bdd5", "lit", "__E_exclusive_or_1", "var", 
 			"T", "F", "__E_var_2", "__E_var_3", 
 		}) nts.get(nt);
@@ -117,7 +148,6 @@ private:
 		o.trim_terminals = false;
 		o.inline_char_classes = true;
 		o.auto_disambiguate = true;
-		o.to_trim = { 4, 5 };
 		return o;
 	}
 	options load_opts() {
@@ -127,20 +157,20 @@ private:
 	idni::prods<char_type, terminal_type> load_prods() {
 		idni::prods<char_type, terminal_type> q,
 			nul(symbol_type{});
-		//       start(6)             => _(4) bdd(7) _(4).
-		q(nt(6), (nt(4)+nt(7)+nt(4)));
-		//       __(5)                => space(1) _(4).
-		q(nt(5), (nt(1)+nt(4)));
-		//       __E___0(8)           => __(5).
-		q(nt(8), (nt(5)));
+		//       start(4)             => _(5) bdd(6) _(5).
+		q(nt(4), (nt(5)+nt(6)+nt(5)));
+		//       __(7)                => space(1) _(5).
+		q(nt(7), (nt(1)+nt(5)));
+		//       __E___0(8)           => __(7).
+		q(nt(8), (nt(7)));
 		//       __E___0(8)           => null.
 		q(nt(8), (nul));
-		//       _(4)                 => __E___0(8).
-		q(nt(4), (nt(8)));
-		//       bdd(7)               => disjunction(9).
-		q(nt(7), (nt(9)));
-		//       bdd(7)               => bdd2(10).
-		q(nt(7), (nt(10)));
+		//       _(5)                 => __E___0(8).
+		q(nt(5), (nt(8)));
+		//       bdd(6)               => disjunction(9).
+		q(nt(6), (nt(9)));
+		//       bdd(6)               => bdd2(10).
+		q(nt(6), (nt(10)));
 		//       bdd2(10)             => conjunction(11).
 		q(nt(10), (nt(11)));
 		//       bdd2(10)             => bdd3(12).
@@ -155,22 +185,22 @@ private:
 		q(nt(14), (nt(16)));
 		//       bdd5(16)             => lit(17).
 		q(nt(16), (nt(17)));
-		//       bdd5(16)             => '(' _(4) bdd(7) _(4) ')'.
-		q(nt(16), (t(1)+nt(4)+nt(7)+nt(4)+t(2)));
-		//       disjunction(9)       => bdd(7) _(4) '|' _(4) bdd2(10).
-		q(nt(9), (nt(7)+nt(4)+t(3)+nt(4)+nt(10)));
-		//       conjunction(11)      => bdd2(10) _(4) '&' _(4) bdd3(12).
-		q(nt(11), (nt(10)+nt(4)+t(4)+nt(4)+nt(12)));
-		//       conjunction(11)      => bdd2(10) __(5) bdd3(12).
-		q(nt(11), (nt(10)+nt(5)+nt(12)));
+		//       bdd5(16)             => '(' _(5) bdd(6) _(5) ')'.
+		q(nt(16), (t(1)+nt(5)+nt(6)+nt(5)+t(2)));
+		//       disjunction(9)       => bdd(6) _(5) '|' _(5) bdd2(10).
+		q(nt(9), (nt(6)+nt(5)+t(3)+nt(5)+nt(10)));
+		//       conjunction(11)      => bdd2(10) _(5) '&' _(5) bdd3(12).
+		q(nt(11), (nt(10)+nt(5)+t(4)+nt(5)+nt(12)));
+		//       conjunction(11)      => bdd2(10) __(7) bdd3(12).
+		q(nt(11), (nt(10)+nt(7)+nt(12)));
 		//       __E_exclusive_or_1(18) => '^'.
 		q(nt(18), (t(5)));
 		//       __E_exclusive_or_1(18) => '+'.
 		q(nt(18), (t(6)));
-		//       exclusive_or(13)     => bdd3(12) _(4) __E_exclusive_or_1(18) _(4) bdd4(14).
-		q(nt(13), (nt(12)+nt(4)+nt(18)+nt(4)+nt(14)));
-		//       negation(15)         => bdd4(14) _(4) '\''.
-		q(nt(15), (nt(14)+nt(4)+t(7)));
+		//       exclusive_or(13)     => bdd3(12) _(5) __E_exclusive_or_1(18) _(5) bdd4(14).
+		q(nt(13), (nt(12)+nt(5)+nt(18)+nt(5)+nt(14)));
+		//       negation(15)         => bdd4(14) _(5) '\''.
+		q(nt(15), (nt(14)+nt(5)+t(7)));
 		//       lit(17)              => var(19).
 		q(nt(17), (nt(19)));
 		//       lit(17)              => T(20).
