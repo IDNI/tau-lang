76177fdfe121c9315ed95ea1c21fe021b6abce42
castrod <david.castroesteban@gmail.com>
2024-04-23 18:58:17 +0200

    improving comments in solver.h

--- src/solver.h
+++ src/solver.h
@@ -11,8 +11,8 @@
 // Contact ohad@idni.org for requesting a permission. This license may be
 // modified over time by the Author.
 
-#ifndef __FIND_SOLUTION_H__
-#define __FIND_SOLUTION_H__
+#ifndef __SOLVER_H__
+#define __SOLVER_H__
 
 #include "tau.h"
 #include "satisfiability.h"
@@ -101,21 +101,33 @@ splitter_set<BAs...> compute_splitter_set(const nlgeqs_clause<BAs...>& clause) {
 	for (size_t i = 0; i < clause.lowers.size(); ++i) {
 		for (size_t j = 0; j < clause.uppers.size(); ++j) {
 			std::variant<BAs...> result = clause.lowers[i];
+			// compute a splitter set element such that it give us a partial solution
+			// for the clause (awaiting computing the remaining part of the solution
+			// depending on the d_j's)
 			for (size_t ii = 0; ii < clause.lowers.size(); ++ii)
-				// (result <= c_ii) <-> (result c_ii' =0)
+				// (result <= c_ii) <-> (result c_ii' = 0)
 				if (std::visit(_leq<BAs...>, result, clause.lowers[ii]))
+					// result <= c_ii, i.e. we must choose c_ii to avoid 0
 					result = std::visit(_and<BAs...>, result, clause.lowers[ii]);
+					// otherwise we must take the negation of c_ii
 				else result = std::visit(_and<BAs...>, result, std::visit(_neg<BAs...>, clause.lowers[ii]));
 
+			// if result d_j = 0 we must choose another d_j
 			result = std::visit(_and<BAs...>, result, clause.uppers[j]);
-			if (!std::visit(_is_zero, result)) continue;
+			if (std::visit(_is_zero, result)) continue;
 
+			// compute the rest ot the splitter set element taking into
+			// account the d_j's to compute the final solution (avoiding 0)
 			for (size_t jj = 0; jj < clause.uppers.size(); ++jj)
-				// (result <= d_jj) <-> (result d_jj' =0)
+				// (result <= d_jj) <-> (result d_jj' = 0)
 				if (std::visit(_leq<BAs...>, clause.uppers[j], clause.uppers[jj]))
+					// result <= d_jj, i.e. we must choose d_jj to avoid 0
 					result = std::visit(_and<BAs...>, result, clause.uppers[jj]);
+					// otherwise we must take the negation of d_jj
 				else result = std::visit(_and<BAs...>, result, std::visit(_neg<BAs...>, clause.uppers[jj]));
 
+			// if the final result is not 0 we add it to the splitter set as
+			// is part of the solution
 			if (!std::visit(_is_zero, result)) ss.insert(result);
 		}
 	}
@@ -125,13 +137,17 @@ splitter_set<BAs...> compute_splitter_set(const nlgeqs_clause<BAs...>& clause) {
 template<typename splitter_t, typename...BAs>
 std::optional<std::variant<BAs...>> solve_clause_using_splitter(nlgeqs_clause<BAs...> clause, splitter_t splitter) {
 	// Following Lemma 3.4 of TABA book we could compute maximal and minilmal solutions.
-	// However, in our case, as we have use the LGRS to remove the equality from
-	// our initial system, we have a=0 and b=1. Thus, there are no maximal or minimal
-	// solutions. We can directly compute the solution using the splitters.
+	// However, in our case, as we have used the LGRS to remove the equality from
+	// our initial system, we have a = 0 and b = 1. Thus, there are no maximal
+	// or minimal solutions. We can directly compute the solution using the splitters.
 
+	// the following code only makes sense if we have maximal and minimal solutions
 	// if (has_clause_maximal_solution(clause)) return ...;
 	// if (has_clause_minimal_solution(clause)) return ...;
 
+	// in our case we can directly compute the solution using the splitter set
+	// and adding the results of the splitter over the splitter set. We should
+	// return {} if we are unable to compute a given splitter for whatever reason.
 	std::variant<BAs...> result;
 	for (auto& split: compute_splitter_set(clause)) {
 		if (auto splitted = splitter(split); splitted.has_value())
@@ -143,6 +159,7 @@ std::optional<std::variant<BAs...>> solve_clause_using_splitter(nlgeqs_clause<BA
 
 template<typename splitter_t, typename...BAs>
 std::optional<std::variant<BAs...>> solve_clause_using_splitter(clause<BAs...> clause, splitter_t splitter) {
+	// split the method in several steps
 	auto var = find_top(clause, is_non_terminal<tau_parser::var, BAs...>)
 		|optional_value_extractor<sp_tau_node<BAs...>>;
 	auto f = find_top(clause, is_non_terminal<tau_parser::bf_eq, BAs...>)
@@ -170,6 +187,8 @@ std::optional<std::variant<BAs...>> solve_clause_using_splitter(clause<BAs...> c
 			| apply_cb<BAs...>)
 		| to_mnf_bf<BAs...>();
 
+	// check consistence f(0)f(1) = 0
+
 	for (auto& neq: select_all(f_0, is_non_terminal<tau_parser::bf_neq, BAs...>)) {
 		auto c_i = neq | tau_parser::bf | only_child_extractor<sp_tau_node<BAs...>>
 			| ba_extractor<BAs...> | optional_value_extractor<sp_tau_node<BAs...>>;
@@ -186,14 +205,17 @@ std::optional<std::variant<BAs...>> solve_clause_using_splitter(clause<BAs...> c
 	return solve_clause_using_splitter(nlg_eqs, splitter);
 }
 
-// Solves an univariate equation using splitters
+// solves an univariate equation using splitters. We tacitely assume that the formula
+// is quatifier-free.
+// for the time beeing, we don't assume that the formula has been simplified, in dnf form,
+// or in any other form. We may drop such assumption in the future.
 template<typename splitter_t, typename...BAs>
 std::optional<std::variant<BAs...>> solve_using_splitter(nso<BAs...> nso, splitter_t splitter) {
-	// convert to DNF
-	// squeeze positives
+	// we apply once definitions.
 	auto nso_with_defs = apply_definitions(nso);
 	auto form = nso_with_defs
 		| repeat_all<step<BAs...>, BAs...>(
+			// also we apply xor,... definitions
 			step<BAs...>(apply_defs<BAs...>))
 		| repeat_each<step<BAs...>, BAs...>(
 			to_dnf_wff<BAs...>
@@ -212,13 +234,16 @@ std::optional<std::variant<BAs...>> solve_using_splitter(nso<BAs...> nso, splitt
 			| simplify_bf<BAs...>
 			| simplify_wff<BAs...>);
 
+	// finally, for each clause we try to find a solution using the splitter
 	for (auto& clause: get_dnf_clauses<tau_parser::wff>(form)) {
-		if (auto onf_clause = to_onf_clause(clause); is_clause_consistent(onf_clause)) {
-			return solve_clause_using_splitter(onf_clause, splitter);
+		if (auto solution = solve_clause_using_splitter(clause, splitter); solution.has_value()) {
+			return solution.value();
 		}
 	}
+	// we were unable to compute a proper splitter at some point which was crucial
+	// for computing the solution, so we return {}. This is a failure.
 	return {};
 }
 
 } // idni::tau namespace
-#endif // __FIND_SOLUTION_H__
+#endif // __SOLVER_H__
