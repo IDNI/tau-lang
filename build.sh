2fd37c9fa3f299a5b0275bfc2fc441a65c25e850
Tomáš Klapka <tomas@idni.org>
2024-05-15 11:18:21 +0200

    move submodule update from cmake to build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 BUILD_TYPE="${1:-Release}"
-if [ "${BUILD_TYPE}" == "Debug" ]
-then
+if [ "${BUILD_TYPE}" == "Debug" ]; then
 	SUFFIX="Debug"
 else
 	SUFFIX="Release"
 fi
 BUILD_DIR="build-${SUFFIX}"
+
+git submodule status | while read -r LINE; do
+	GIT_SUBMOD=$(echo $LINE | awk '{print $2}')
+	if [[ $LINE == -* ]]; then
+		echo "Initializing submodule $GIT_SUBMOD"
+		git submodule update --init --recursive $GIT_SUBMOD
+	else
+		echo "Submodule ${GIT_SUBMOD} is already initialized"
+	fi
+done
 
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
@@ -15,22 +24,22 @@ rm -f ./CMakeCache.txt
 
 NINJA_BIN="$(which ninja 2>&1)";
 if [ $? -ne 0 ]; then
-    NINJA_BIN="$(which ninja-build 2>&1)";
-    if [ $? -ne 0 ]; then
-        NINJA_BIN="";
-    fi
+	NINJA_BIN="$(which ninja-build 2>&1)";
+	if [ $? -ne 0 ]; then
+		NINJA_BIN="";
+	fi
 fi
 
 if [ -z $NINJA_BIN ]; then
-  echo "Using make build system"
-  cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" ${@:2}
-  cmake --build . -- -j5
-  STATUS=$?
+	echo "Using make build system"
+	cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" ${@:2}
+	cmake --build . -- -j5
+	STATUS=$?
 else
-  echo "Using Ninja build system"
-  cmake .. -G Ninja -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" ${@:2}
-  ninja
-  STATUS=$?
+	echo "Using Ninja build system"
+	cmake .. -G Ninja -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" ${@:2}
+	ninja
+	STATUS=$?
 fi
 
 cd ..
