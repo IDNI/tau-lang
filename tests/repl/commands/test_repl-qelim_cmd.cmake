3bd3550b7f9c972181b676cb8dbd906aeffb1978
castrod <david.castroesteban@gmail.com>
2024-05-22 12:25:03 +0200

    reviewwing qelim repl tests

--- tests/repl/commands/test_repl-qelim_cmd.cmake
+++ tests/repl/commands/test_repl-qelim_cmd.cmake
@@ -2,5 +2,6 @@
 # qelim command
 #
 
-add_repl_test(qelim_cmd-form "qelim all x ex y x=y" "!\\(ex x F\\)")
-add_repl_test(qelim_cmd-memory "mem all x ex y x=y. qelim %0" "!\\(ex x F\\)")
+add_repl_test(qelim_cmd-form "qelim T" ": T")
+add_repl_test(qelim_cmd-abs_mem "mem T. qelim %0" "T")
+add_repl_test(qelim_cmd-rel_mem "mem T. qelim %-0" "T")
