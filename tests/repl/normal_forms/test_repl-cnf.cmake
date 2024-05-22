4073d421869f28ef42e984cc64dd1cf4c215fffb
castrod <david.castroesteban@gmail.com>
2024-05-22 12:13:18 +0200

    reviewing repl tests

--- tests/repl/normal_forms/test_repl-cnf.cmake
+++ tests/repl/normal_forms/test_repl-cnf.cmake
@@ -6,31 +6,16 @@
 # wff
 #
 
-# T
 add_repl_test(normal_forms-cnf_wff_T "cnf T" ": T")
-
-# F
 add_repl_test(normal_forms-cnf_wff_F "cnf F" ": F")
-
-# (X=0 || (Y=0 && Z=0))
 add_repl_test(normal_forms-cnf_wff_dist_right "cnf (X=0 || (Y=0 && Z=0))" ": \\(X = 0 \\|\\| Y = 0\\) && \\(X = 0 \\|\\| Z = 0\\)")
-
-# ((X=0 && Y=0) | Z=0)
 add_repl_test(normal_forms-cnf_wff_dist_left "cnf ((X=0 && Y=0) || Z=0)" ": \\(X = 0 \\|\\| Z = 0\\) && \\(Y = 0 \\|\\| Z = 0\\)")
 
 #
 # bf
 #
 
-# 0
 add_repl_test(normal_forms-cnf_bf_1 "cnf 1" ": 1")
-
-# 1
 add_repl_test(normal_forms-cnf_bf_0 "cnf 0" ": 0")
-
-# TODO (HIGH) review the following tests (they are flaky due to the use of std::set)
-# (X | (Y & Z))
 add_repl_test(normal_forms-cnf_bf_dist_right "cnf (X | (Y & Z))" ": \\(. \\| .\\) & \\(. \\| .\\)|")
-
-# ((X & Y) | Z)
 add_repl_test(normal_forms-cnf_bf_dist_left "cnf ((X & Y) | Z)" ": \\(. \\| .\\) & \\(. \\| .\\)")
