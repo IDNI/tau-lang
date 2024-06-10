7f84f1ff940bb4e392d8c0cc4856d9b8e4972e67
castrod <david.castroesteban@gmail.com>
2024-06-10 05:32:25 +0200

    improving script

--- demos/run.sh
+++ demos/run.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
+# clear the terminal
+clear
+
+# check parameter is passed
+if [ -z "$1" ]; then
+  echo "Usage: $0 <demo_file>"
+  exit 1
+fi
+
+# check that the demo file exists
+if [ ! -f $1 ]; then
+  echo "File $1 not found!"
+  exit 1
+fi
+
+# check that the tau executable exists
+if [ ! -f ./../build-Release/tau ]; then
+  echo "Tau executable not found! Please compile the project first (in Release mode)."
+  exit 1
+fi
+
 pipe=$(mktemp -u)
 mkfifo $pipe
+
+echo "You are executing a Tau demo script. Please press any key to contionue step by step over it."
 
 # wait 10 secs till tau is up and running
 (sleep 10 && while IFS= read -r line
 do
-  echo "$line"
-  # execute commands every 1 seconds
-  # use '#' to get extra time
-  sleep 1
+  # wait till the user press return
+  read -s -n1 < /dev/tty
+  # ignore ## comments
+  echo "$line" | grep -v "##"
 done) < $1 > $pipe &
 
 ./../build-Release/tau < $pipe
