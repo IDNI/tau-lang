1b7ac844cc14a41996164c01065e65fadbda1246
Lucca Tiemens <lucca@tiemens.de>
2024-07-12 17:46:21 +0200

    Added splitter for SBFs.

--- tests/unit/test_hbdd.cpp
+++ tests/unit/test_hbdd.cpp
@@ -76,3 +76,36 @@ TEST_SUITE("operator~") {
 		CHECK( (~get_zero<Bool>()) == get_one<Bool>() );
 	}
 }
+
+TEST_SUITE("BDD_Splitter") {
+	TEST_CASE("DNF_clause_deletion1") {
+		bdd_init<Bool>();
+		auto a1 = bdd_handle<Bool>::bit(true, 1);
+		auto a2 = bdd_handle<Bool>::bit(true, 2);
+		auto a3 = bdd_handle<Bool>::bit(true, 3);
+
+		auto d1 = a1 | a2 | a3;
+		CHECK(splitter(d1) == (a1 | a2));
+	}
+
+	TEST_CASE("DNF_clause_deletion2") {
+		bdd_init<Bool>();
+		auto a1 = bdd_handle<Bool>::bit(false, 1);
+		auto a2 = bdd_handle<Bool>::bit(false, 2);
+		auto a3 = bdd_handle<Bool>::bit(false, 3);
+
+		auto d1 = a1 | a2 | a3;
+		CHECK(splitter(d1) == (a1 | a2));
+	}
+
+	TEST_CASE("single_DNF_clause") {
+		bdd_init<Bool>();
+		auto a1 = bdd_handle<Bool>::bit(true, 1);
+		auto a2 = bdd_handle<Bool>::bit(true, 2);
+		auto a3 = bdd_handle<Bool>::bit(true, 3);
+		auto a4 = bdd_handle<Bool>::bit(true, 4);
+
+		auto d1 = a1 & a2 & a3;
+		CHECK(splitter(d1) == (d1 & a4));
+	}
+}
