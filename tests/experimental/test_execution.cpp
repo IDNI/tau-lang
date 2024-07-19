48abc298cb7c276346fc05d25d26f24d73067e0f
castrod <david.castroesteban@gmail.com>
2024-07-19 20:29:37 +0200

    speccing out experimental execution API

---
+++ tests/experimental/test_execution.cpp
@@ -0,0 +1,31 @@
+// LICENSE
+// This software is free for use and redistribution while including this
+// license notice, unless:
+// 1. is used for commercial or non-personal purposes, or
+// 2. used for a product which includes or associated with a blockchain or other
+// decentralized database technology, or
+// 3. used for a product which includes or associated with the issuance or use
+// of cryptographic or electronic currencies/coins/tokens.
+// On all of the mentioned cases, an explicit and written permission is required
+// from the Author (Ohad Asor).
+// Contact ohad@idni.org for requesting a permission. This license may be
+// modified over time by the Author.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+
+#include "experimental/execution.h"
+
+using namespace std;
+using namespace idni::tau;
+using namespace idni::tau::experimental;
+
+namespace testing = doctest;
+
+TEST_SUITE("new execution API") {
+
+	TEST_CASE("new execution API test case") {
+		CHECK(true);
+	}
+}
