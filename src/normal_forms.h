11d397ffc5ee834e55e04339d3d34146c2473d60
castrod <david.castroesteban@gmail.com>
2024-08-16 16:16:29 +0200

    adding benchmarking infrastructure and tests

--- src/normal_forms.h
+++ src/normal_forms.h
@@ -1540,10 +1540,10 @@ private:
 
 	nso<BAs...> traverse(const bdd_path& path, const literals& remaining, const nso<BAs...>& form) const {
 		// we only cache results in release mode
-		#ifdef CACHE
+		#ifdef TAU_CACHE
 		static std::map<std::tuple<bdd_path, literals, nso<BAs...>>, nso<BAs...>> cache;
 		if (auto it = cache.find({path, remaining, form}); it != cache.end()) return it->second;
-		#endif // CACHE
+		#endif // TAU_CACHE
 
 		if (remaining.empty()) return bdd_path_to_snf(path, form);
 
@@ -1554,9 +1554,9 @@ private:
 
 		if (f == _F<BAs...> && t == _F<BAs...>) {
 			// we only cache results in release mode
-			#ifdef CACHE
+			#ifdef TAU_CACHE
 			cache[{path, remaining, form}] = _F<BAs...>;
-			#endif // CACHE
+			#endif // TAU_CACHE
 			return _F<BAs...>;
 		}
 
@@ -1577,24 +1577,24 @@ private:
 		}
 
 		if (f_snf == _F<BAs...>) {
-			#ifdef CACHE
+			#ifdef TAU_CACHE
 			cache[{path, remaining, form}] = t_snf;
-			#endif // CACHE
+			#endif // TAU_CACHE
 			return t_snf;
 		}
 
 		if (t_snf == _F<BAs...>) {
-			#ifdef CACHE
+			#ifdef TAU_CACHE
 			cache[{path, remaining, form}] = f_snf;
-			#endif // CACHE
+			#endif // TAU_CACHE
 			return f_snf;
 		}
 
 		auto result = build_wff_or(t_snf, f_snf);
 		// we only cache results in release mode
-		#ifdef CACHE
+		#ifdef TAU_CACHE
 		cache[{path, remaining, form}] = result;
-		#endif // CACHE
+		#endif // TAU_CACHE
 		return result;
 	}
 
