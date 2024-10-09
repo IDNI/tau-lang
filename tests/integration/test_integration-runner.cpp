b26fb3e47de4ae6662b6981f58bcb3ff24a49be5
castrod <david.castroesteban@gmail.com>
2024-10-09 11:30:11 +0200

    reorganizing test infrastructure to add test with inputs, inputs and outputs.

--- tests/integration/test_integration-runner.cpp
+++ tests/integration/test_integration-runner.cpp
@@ -48,91 +48,106 @@ std::string random_file(const std::string& extension = ".out", const std::string
     return oss.str();
 }
 
-TEST_SUITE("only outputs") {
+template<typename...BAs>
+struct output_bdd_console {
 
-	// In this test suite we test the interpreter with only outputs in the Tau
-	// specifications, no inputs are provided at all. The outputs are written
-	// to the console.
+	bool write(const assignment<BAs...>& outputs) {
+		// for each stream in out.streams, write the value from the solution
+		for (const auto& [var, value]: outputs)
+			std::cout << var << " <- " << value << "\n";
+		return true; // success (always)
+	}
 
-	template<typename...BAs>
-	struct output_bdd_console {
+	std::optional<type> type_of(const nso<BAs...>&) {
+		return { "bdd" }; // bdd (always)
+	}
+};
 
-		bool write(const assignment<BAs...>& outputs) {
-			// for each stream in out.streams, write the value from the solution
-			for (const auto& [var, value]: outputs)
-				std::cout << var << " <- " << value << "\n";
-			return true; // success (always)
-		}
+template<typename...BAs>
+struct input_bdd_vector {
 
-		std::optional<type> type_of(const nso<BAs...>&) {
-			return { "bdd" }; // bdd (always)
-		}
-	};
+	std::optional<assignment<BAs...>> read() {
+		if (inputs.empty()) return { assignment<BAs...>{} };
+		if (current == inputs.size()) return {};
+		return inputs[current++];
+	}
 
-	template<typename...BAs>
-	struct input_bdd_vector {
+	std::optional<type> type_of(const nso<BAs...>&) {
+		return { "bdd" }; // bdd (always)
+	}
 
-		std::optional<assignment<BAs...>> read() {
-			if (inputs.empty()) return { assignment<BAs...>{} };
-			if (current == inputs.size()) return {};
-			return inputs[current++];
-		}
+	std::vector<assignment<BAs...>> inputs;
+	size_t current = 0;
+};
 
-		std::optional<type> type_of(const nso<BAs...>&) {
-			return { "bdd" }; // bdd (always)
-		}
+assignment<tau_ba<bdd_binding>, bdd_binding> run_test(const char* sample,
+		input_bdd_vector<tau_ba<bdd_binding>, bdd_binding>& inputs,
+		output_bdd_console<tau_ba<bdd_binding>, bdd_binding>& outputs,
+		const size_t& times) {
+	auto sample_src = make_tau_source(sample);
+	auto phi_inf = make_nso_rr_using_factory<tau_ba<bdd_binding>, bdd_binding>(sample_src).main;
 
-		std::vector<assignment<BAs...>> inputs;
-		size_t current = 0;
-	};
+	#ifdef DEBUG
+	std::cout << "------------------------------------------------------\n";
+	std::cout << "run_test/sample: " << sample << "\n";
+	#endif // DEBUG
 
-	assignment<tau_ba<bdd_binding>, bdd_binding> run_test(const char* sample,
-			const size_t& times) {
-		bdd_init<Bool>();
-		auto sample_src = make_tau_source(sample);
-		auto phi_inf = make_nso_rr_using_factory<tau_ba<bdd_binding>, bdd_binding>(sample_src).main;
+	auto runner = make_interpreter(phi_inf, inputs, outputs).value();
+
+	// we read the inputs only once (they are always empty in this test suite)
+	auto in = inputs.read();
+
+	for (size_t i = 0; i < times; ++i) {
+		// we execute the i-th step
+		auto out = runner.step(in.value());
 
 		#ifdef DEBUG
-		std::cout << "------------------------------------------------------\n";
-		std::cout << "run_test/sample: " << sample << "\n";
-		#endif // DEBUG
+		if (out.size() == 0) {
+			std::cout << "run_test/output[" << i << "]: {}"; // no output
+			runner.memory.clear();
+			break;
+		}
 
-		// prepare inputs, outputs and the interpreter
-		input_bdd_vector<tau_ba<bdd_binding>, bdd_binding> inputs;
-		output_bdd_console<tau_ba<bdd_binding>, bdd_binding> outputs;
-		auto runner = make_interpreter(phi_inf, inputs, outputs).value();
-
-		// we read the inputs only once (they are always empty in this test suite)
-		auto in = inputs.read();
-
-		for (size_t i = 0; i < times; ++i) {
-			// we execute the i-th step
-			auto out = runner.step(in.value());
-
-			#ifdef DEBUG
-			if (out.size() == 0) {
-				std::cout << "run_test/output[" << i << "]: {}"; // no output
+		std::cout << "run_test/output[" << i << "]: ";
+		for (const auto& [var, value]: out) {
+			std::cout << var << " <- " << value << " ... ";
+			if (auto io_vars = find_top(value, is_non_terminal<tau_parser::io_var, tau_ba<bdd_binding>, bdd_binding>); io_vars) {
+				std::cout << "run_test/output[", i, "]: unexpected io_var ", io_vars.value();
 				runner.memory.clear();
 				break;
 			}
-
-			std::cout << "run_test/output[" << i << "]: ";
-			for (const auto& [var, value]: out) {
-				std::cout << var << " <- " << value << " ... ";
-				if (auto io_vars = find_top(value, is_non_terminal<tau_parser::io_var, tau_ba<bdd_binding>, bdd_binding>); io_vars) {
-					std::cout << "run_test/output[", i, "]: unexpected io_var ", io_vars.value();
-					runner.memory.clear();
-					break;
-				}
-			}
-			std::cout << "\n";
-			#endif // DEBUG
-
-			if (runner.memory.empty()) break;
 		}
+		std::cout << "\n";
+		#endif // DEBUG
 
-		return runner.memory;
+		if (runner.memory.empty()) break;
 	}
+
+	return runner.memory;
+}
+
+assignment<tau_ba<bdd_binding>, bdd_binding> run_test(const char* sample,
+		const size_t& times) {
+	input_bdd_vector<tau_ba<bdd_binding>, bdd_binding> inputs;
+	output_bdd_console<tau_ba<bdd_binding>, bdd_binding> outputs;
+	return run_test(sample, inputs, outputs, times);
+}
+
+assignment<tau_ba<bdd_binding>, bdd_binding> run_test(const char* sample,
+		input_bdd_vector<tau_ba<bdd_binding>, bdd_binding>& inputs,
+		const size_t& times) {
+	output_bdd_console<tau_ba<bdd_binding>, bdd_binding> outputs;
+	return run_test(sample, inputs, outputs, times);
+}
+
+assignment<tau_ba<bdd_binding>, bdd_binding> run_test(const char* sample,
+		output_bdd_console<tau_ba<bdd_binding>, bdd_binding>& outputs,
+		const size_t& times) {
+	output_bdd_console<tau_ba<bdd_binding>, bdd_binding> outputs;
+	return run_test(sample, inputs, outputs, times);
+}
+
+TEST_SUITE("configuration") {
 
 	TEST_CASE("configuration") {
 		// initialize the bdd library
@@ -142,6 +157,13 @@ TEST_SUITE("only outputs") {
 		add_console_log(cout, keywords::format =
 			expressions::stream << expressions::smessage);
 	}
+}
+
+TEST_SUITE("only outputs") {
+
+	// In this test suite we test the interpreter with only outputs in the Tau
+	// specifications, no inputs are provided at all. The outputs are written
+	// to the console.
 
 	// coonstant test cases
 
@@ -167,6 +189,18 @@ TEST_SUITE("only outputs") {
 		const char* sample = "o1[0] = {bdd: a}.";
 		auto memory = run_test(sample, 3);
 		CHECK ( !memory.empty() );
+	}
+
+	// TODO (HIGH) check with Ohad the proper output in this case.
+	// The spec is T, i.e. all outputs are valid. However, due to our inner
+	// implementation, we are not defining outputs as we have T in the spec.
+	// and hence, we are not able to provide outputs.
+	// One possibility is return an optional instead of an assignment and
+	// return an optional with an empty assigment in this case.
+	TEST_CASE("o1[t] & o1[t]'= 0") {
+		const char* sample = "o1[t] | o1[t]' = 0.";
+		auto memory = run_test(sample, 2);
+		CHECK ( memory.empty() );
 	}
 
 	TEST_CASE("o1[0] = 1 && o1[t] = o1[t-1]") {
@@ -225,4 +259,17 @@ TEST_SUITE("only outputs") {
 		auto memory = run_test(sample, 8);
 		CHECK ( !memory.empty() );
 	}
+}
+
+
+TEST_SUITE("with inputs") {
+
+}
+
+TEST_SUITE("test inputs") {
+
+}
+
+TEST_SUITE("test outputs") {
+
 }
