2a998c3c9ffb5da51f0ccbca819898fde691c48f
Lucca Tiemens <lucca@tiemens.de>
2024-11-13 11:43:24 +0100

    Fixed tests involving non-closed Tau formula constants.

--- tests/integration/test_integration-splitter2.cpp
+++ tests/integration/test_integration-splitter2.cpp
@@ -31,18 +31,19 @@ namespace testing = doctest;
 
 TEST_SUITE("Tau_splitter_tau_coeff") {
 TEST_CASE("Tau_splitter_tau_coeff1") {
-	const char *src = "{xy = 0.} v != 0.";
+	const char *src = "{o1[t]o2[t] = 0.} v != 0.";
 	auto fm = make_nso_rr_using_factory<tau_ba<bdd_test>, bdd_test>(src).value().main;
 	auto s = tau_splitter(fm, splitter_type::upper);
 	stringstream ss; ss << s;
-	CHECK(ss.str() == "{ xy = 0. } ({ xy = 0. } v)' = 0");
+	CHECK(ss.str() == "{ o1[t]o2[t] = 0. } ({ o1[t]o2[t] = 0. } v)' = 0");
 }
 
 TEST_CASE("Tau_splitter_tau_coeff2") {
-	const char *src = "{x|y = 0.}&v = 0.";
+	const char *src = "{o1[t]|o2[t] = 0.}&v = 0.";
 	auto fm = make_nso_rr_using_factory<tau_ba<bdd_test>, bdd_test>(src).value().main;
 	auto s = tau_splitter(fm, splitter_type::upper);
 	stringstream ss; ss << s;
-	CHECK(ss.str() == "{ x|y = 0. } v = 0 && v{ !(x = 0 && y = 0). } = 0");
+	CHECK((ss.str() == "{ o1[t]|o2[t] = 0. } v = 0 && v{ !(always o1[t] = 0 && o2[t] = 0). } = 0"
+		|| ss.str() == "{ o1[t]|o2[t] = 0. } v = 0 && v{ !(always o2[t] = 0 && o1[t] = 0). } = 0"));
 }
 }
