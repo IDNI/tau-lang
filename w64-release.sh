a0dd5c5a66a4615f17b7ff61e63daad965588079
tklip <tomas@idni.org>
2024-11-15 18:43:27 +0100

    cpack: DEB;RPM;ZIP;NSIS (missing PATH setting)

--- w64-release.sh
+++ w64-release.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./build.sh Release -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw-w64-x86_64.cmake $@
+./build.sh Release -DTAU_WINDOWS_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw-w64-x86_64.cmake $@
+cd ./build-Release
+cpack -C Release
+cd ..
+
+./build.sh Release -DTAU_WINDOWS_ZIP_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw-w64-x86_64.cmake $@
+cd ./build-Release
+cpack -C Release
+cd ..
