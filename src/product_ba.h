ef468a3a071dd7ec79f91149b20660e2accd651a
castrod <david.castroesteban@gmail.com>
2025-01-09 11:20:54 +0100

    removing ba_ref (unused)

--- src/product_ba.h
+++ src/product_ba.h
@@ -47,34 +47,4 @@ struct product_ba: std::tuple<BAS...> {
 	}
 };
 
-// reference boolean algebra
-template <typename B>
-struct ba_ref {
-	//TODO add proper builders/constructors to
-	// all the classes involved in this algebra
-	struct var {
-		size_t id;
-		int offset;
-	};
-
-	struct ref {
-		// id of the cbf or wff that this ref belongs to
-		std::size_t id;
-		// TODO check if maybe a pair of variants is better?
-		std::list<std::variant<var, size_t>> idxs;
-		std::list<std::variant<var, B>> args;
-	};
-
-	// each ref corresponds to a unique bdd variable
-	static std::map<ref, size_t> refs_map;
-	hbdd<B> h;
-
-	explicit ba_ref(hbdd<B> h): h(h) {}
-
-	ba_ref<B> operator~() {	return {~h}; }
-	ba_ref<B> operator&(ba_ref<B>& that) { return {h & that.h}; }
-	ba_ref<B> operator|(ba_ref<B>& that) { return {h | that.h}; }
-	ba_ref<B> operator^(ba_ref<B>& that) { return {h ^ that.h};	}
-};
-
 #endif // __BA_H__
