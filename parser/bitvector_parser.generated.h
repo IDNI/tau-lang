39ba665168ffa1f0a538dbe3ef387af31b63ef92
castrod <david.castroesteban@gmail.com>
2025-02-17 06:44:23 +0100

    updating names of nonterminals to avoid conflitcs with cpp

--- parser/bitvector_parser.generated.h
+++ parser/bitvector_parser.generated.h
@@ -12,9 +12,9 @@ using char_type     = char;
 using terminal_type = char;
 
 inline std::vector<std::string> symbol_names{
-	"", "space", "alpha", "digit", "start", "_", "bitvector", "uint", "int", "ulong", 
-	"long", "bits", "sign", "minus", "plus", "__E_plus_0", "unsigned", "__E_uint_1", "__E_ulong_2", "bit", 
-	"__E_bits_3", "__E_bits_4", "zero", "one", 
+	"", "space", "alpha", "digit", "start", "_", "bitvector", "_uint", "_int", "_ulong", 
+	"_long", "_bits", "sign", "minus", "plus", "__E_plus_0", "_unsigned", "__E__uint_1", "__E__ulong_2", "__E__unsigned_3", 
+	"_digit", "__E__unsigned_4", "_bit", "__E__bits_5", "__E__bits_6", "zero", "one", 
 };
 
 inline ::idni::nonterminals<char_type, terminal_type> nts{symbol_names};
@@ -58,15 +58,15 @@ inline idni::prods<char_type, terminal_type>& productions() {
 	#define NT(x) (idni::prods<char_type, terminal_type>{ nts(x) })
 //G0:   start(4)             => _(5) bitvector(6) _(5).
 	p(NT(4), (NT(5)+NT(6)+NT(5)));
-//G1:   bitvector(6)         => uint(7).
+//G1:   bitvector(6)         => _uint(7).
 	p(NT(6), (NT(7)));
-//G2:   bitvector(6)         => int(8).
+//G2:   bitvector(6)         => _int(8).
 	p(NT(6), (NT(8)));
-//G3:   bitvector(6)         => ulong(9).
+//G3:   bitvector(6)         => _ulong(9).
 	p(NT(6), (NT(9)));
-//G4:   bitvector(6)         => long(10).
+//G4:   bitvector(6)         => _long(10).
 	p(NT(6), (NT(10)));
-//G5:   bitvector(6)         => bits(11).
+//G5:   bitvector(6)         => _bits(11).
 	p(NT(6), (NT(11)));
 //G6:   sign(12)             => minus(13).
 	p(NT(12), (NT(13)));
@@ -80,38 +80,46 @@ inline idni::prods<char_type, terminal_type>& productions() {
 	p(NT(15), (nul));
 //G11:  plus(14)             => __E_plus_0(15).
 	p(NT(14), (NT(15)));
-//G12:  int(8)               => sign(12) _(5) unsigned(16).
+//G12:  _int(8)              => sign(12) _(5) _unsigned(16).
 	p(NT(8), (NT(12)+NT(5)+NT(16)));
-//G13:  long(10)             => sign(12) _(5) unsigned(16) _(5) 'l'.
+//G13:  _long(10)            => sign(12) _(5) _unsigned(16) _(5) 'l'.
 	p(NT(10), (NT(12)+NT(5)+NT(16)+NT(5)+T(3)));
-//G14:  __E_uint_1(17)       => plus(14).
+//G14:  __E__uint_1(17)      => plus(14).
 	p(NT(17), (NT(14)));
-//G15:  __E_uint_1(17)       => null.
+//G15:  __E__uint_1(17)      => null.
 	p(NT(17), (nul));
-//G16:  uint(7)              => __E_uint_1(17) _(5) unsigned(16) _(5) 'u'.
+//G16:  _uint(7)             => __E__uint_1(17) _(5) _unsigned(16) _(5) 'u'.
 	p(NT(7), (NT(17)+NT(5)+NT(16)+NT(5)+T(4)));
-//G17:  __E_ulong_2(18)      => plus(14).
+//G17:  __E__ulong_2(18)     => plus(14).
 	p(NT(18), (NT(14)));
-//G18:  __E_ulong_2(18)      => null.
+//G18:  __E__ulong_2(18)     => null.
 	p(NT(18), (nul));
-//G19:  ulong(9)             => __E_ulong_2(18) _(5) unsigned(16) _(5) 'u' 'l'.
+//G19:  _ulong(9)            => __E__ulong_2(18) _(5) _unsigned(16) _(5) 'u' 'l'.
 	p(NT(9), (NT(18)+NT(5)+NT(16)+NT(5)+T(4)+T(3)));
-//G20:  __E_bits_3(20)       => _(5) bit(19).
-	p(NT(20), (NT(5)+NT(19)));
-//G21:  __E_bits_4(21)       => null.
+//G20:  __E__unsigned_3(19)  => _digit(20).
+	p(NT(19), (NT(20)));
+//G21:  __E__unsigned_4(21)  => null.
 	p(NT(21), (nul));
-//G22:  __E_bits_4(21)       => __E_bits_3(20) __E_bits_4(21).
-	p(NT(21), (NT(20)+NT(21)));
-//G23:  bits(11)             => bit(19) __E_bits_4(21) 'b'.
-	p(NT(11), (NT(19)+NT(21)+T(5)));
-//G24:  zero(22)             => '0'.
-	p(NT(22), (T(6)));
-//G25:  bit(19)              => zero(22).
-	p(NT(19), (NT(22)));
-//G26:  one(23)              => '1'.
-	p(NT(23), (T(7)));
-//G27:  bit(19)              => one(23).
-	p(NT(19), (NT(23)));
+//G22:  __E__unsigned_4(21)  => __E__unsigned_3(19) __E__unsigned_4(21).
+	p(NT(21), (NT(19)+NT(21)));
+//G23:  _unsigned(16)        => digit(3) __E__unsigned_4(21).
+	p(NT(16), (NT(3)+NT(21)));
+//G24:  __E__bits_5(23)      => _(5) _bit(22).
+	p(NT(23), (NT(5)+NT(22)));
+//G25:  __E__bits_6(24)      => null.
+	p(NT(24), (nul));
+//G26:  __E__bits_6(24)      => __E__bits_5(23) __E__bits_6(24).
+	p(NT(24), (NT(23)+NT(24)));
+//G27:  _bits(11)            => _bit(22) __E__bits_6(24) 'b'.
+	p(NT(11), (NT(22)+NT(24)+T(5)));
+//G28:  zero(25)             => '0'.
+	p(NT(25), (T(6)));
+//G29:  _bit(22)             => zero(25).
+	p(NT(22), (NT(25)));
+//G30:  one(26)              => '1'.
+	p(NT(26), (T(7)));
+//G31:  _bit(22)             => one(26).
+	p(NT(22), (NT(26)));
 	#undef T
 	#undef NT
 	return loaded = true, p;
@@ -124,9 +132,9 @@ inline ::idni::grammar<char_type, terminal_type> grammar(
 
 struct bitvector_parser : public idni::parser<char, char> {
 	enum nonterminal {
-		nul, space, alpha, digit, start, _, bitvector, uint, int, ulong, 
-		long, bits, sign, minus, plus, __E_plus_0, unsigned, __E_uint_1, __E_ulong_2, bit, 
-		__E_bits_3, __E_bits_4, zero, one, 
+		nul, space, alpha, digit, start, _, bitvector, _uint, _int, _ulong, 
+		_long, _bits, sign, minus, plus, __E_plus_0, _unsigned, __E__uint_1, __E__ulong_2, __E__unsigned_3, 
+		_digit, __E__unsigned_4, _bit, __E__bits_5, __E__bits_6, zero, one, 
 	};
 	static bitvector_parser& instance() {
 		static bitvector_parser inst;
