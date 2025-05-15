ce052f028d8b73d886257c2bb23cd8e4d29fe8c0
tklip <tomas@idni.org>
2025-05-15 11:17:18 +0200

    - enable trace logs in debug tests by default; - logging::level() to get the current level

--- tests/test_init.h
+++ tests/test_init.h
@@ -1,9 +1,11 @@
 // To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.txt
 
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 
 #include <cassert>
 #include "doctest.h"
+#include "defs.h"
+#include "logging.h"
 
 using namespace std;
 
@@ -15,3 +17,12 @@ using namespace idni::tau_lang;
 
 namespace testing = doctest;
 
+int main(int argc, char** argv) {
+	DBG(std::cout << "Logging severity level: " << logging::level() << "\n";)
+#ifdef TAU_LOG_TRACE_TESTS
+	logging::trace();
+	std::cout << "Logging severity level set: " << logging::level() << "\n";
+#endif // TAU_LOG_TRACE_TESTS
+
+	return doctest::Context(argc, argv).run();
+}
