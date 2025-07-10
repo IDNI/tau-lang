33cdc161e9e852c3ea8f81c449bdb5adf82a06d1
Lucca Tiemens <lucca@tiemens.de>
2025-07-10 17:38:35 +0200

    Corrections for missing bf node in constant creation.

--- src/boolean_algebras/nso_ba.tmpl.h
+++ src/boolean_algebras/nso_ba.tmpl.h
@@ -17,7 +17,7 @@ const tree<node<BAs...>>& operator&(const tree<node<BAs...>>& lt,
 	auto bf_constant_and = [](const auto& lt, const auto& rt) {
 		DBG(assert(lt.get_ba_type() == rt.get_ba_type()
 						&& lt.get_ba_type() > 0);)
-		return tau::get_ba_constant(
+		return tau::build_bf_ba_constant(
 				lt.get_ba_constant() & rt.get_ba_constant(),
 				lt.get_ba_type());
 	};
@@ -51,7 +51,7 @@ const tree<node<BAs...>>& operator|(const tree<node<BAs...>>& lt,
 	auto bf_constant_or = [](const auto& lt, const auto& rt) {
 		DBG(assert(lt.get_ba_type() == rt.get_ba_type()
 						&& lt.get_ba_type() > 0);)
-		return tau::get_ba_constant(
+		return tau::build_bf_ba_constant(
 				lt.get_ba_constant() | rt.get_ba_constant(),
 				lt.get_ba_type());
 	};
@@ -76,7 +76,7 @@ const tree<node<BAs...>>& operator~(const tree<node<BAs...>>& lt) {
 
 	auto bf_constant_neg = [](const auto& lt) {
 		DBG(assert(lt.get_ba_type() > 0);)
-		return tau::get_ba_constant(~lt.get_ba_constant(),
+		return tau::build_bf_ba_constant(~lt.get_ba_constant(),
 					     lt.get_ba_type());
 	};
 
@@ -108,7 +108,7 @@ const tree<node<BAs...>>& operator^(const tree<node<BAs...>>& lt,
 	auto bf_constant_xor = [](const auto& lt, const auto& rt) {
 		DBG(assert(lt.get_ba_type() == rt.get_ba_type()
 						&& lt.get_ba_type() > 0);)
-		return tau::get_ba_constant(
+		return tau::build_bf_ba_constant(
 				lt.get_ba_constant() ^ rt.get_ba_constant(),
 				lt.get_ba_type());
 	};
@@ -249,7 +249,7 @@ const tree<node<BAs...>>& splitter(const tree<node<BAs...>>& t,
 	};
 	DBG(assert(t.is_ba_constant());)
 	return tree<node<BAs...>>::get(
-		tree<node<BAs...>>::get_ba_constant(
+		tree<node<BAs...>>::build_bf_ba_constant(
 			std::visit(_splitter, t.get_ba_constant()),
 			t.get_ba_type()));
 }
