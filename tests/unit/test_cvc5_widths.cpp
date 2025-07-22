754565d97ba3e389e60cc4c47b40096350902358
castrod <david.castroesteban@gmail.com>
2025-07-22 14:56:19 +0200

    adding ctes width annotator tests

--- tests/unit/test_cvc5_widths.cpp
+++ tests/unit/test_cvc5_widths.cpp
@@ -13,7 +13,7 @@ using namespace boost::log;
 
 namespace testing = doctest;
 
-TEST_SUITE("cvc5_widths") {
+TEST_SUITE("annotate bv widths") {
 
 	TEST_CASE("no bitvector vars: case 1") {
 		const char* sample = "x = 1";
@@ -173,4 +173,95 @@ TEST_SUITE("cvc5_widths") {
 		BOOST_LOG_TRIVIAL(info) << "Annotated: " << annotated;
 		#endif // DEBUG
 	}
+}
+
+TEST_SUITE("annotate bv ctes") {
+
+	TEST_CASE("no bitvector ctes: case 1") {
+		const char* sample = "x = 1";
+		auto src = make_tau_source(sample, { .start = tau_parser::wff });
+		auto expr = make_statement(src);
+		auto annotated = annotate_bv_ctes(expr);
+		CHECK( select_all(expr, is_non_terminal<tau_parser::bv_type, Bool>).size() == 0 );
+		CHECK( select_all(annotated, is_non_terminal<tau_parser::bv_type, Bool>).size() == 0 );
+		CHECK( annotated == expr );
+		
+		#ifdef DEBUG
+		BOOST_LOG_TRIVIAL(info) << "Expr: " << expr;
+		BOOST_LOG_TRIVIAL(info) << "Annotated: " << annotated;
+		#endif // DEBUG
+	}
+
+	TEST_CASE("no bitvector ctes: case 2") {
+		const char* sample = "all x x = 1";
+		auto src = make_tau_source(sample, { .start = tau_parser::wff });
+		auto expr = make_statement(src);
+		auto annotated = annotate_bv_ctes(expr);
+		CHECK( select_all(expr, is_non_terminal<tau_parser::bv_type, Bool>).size() == 0 );
+		CHECK( select_all(annotated, is_non_terminal<tau_parser::bv_type, Bool>).size() == 0 );
+		CHECK( annotated == expr );
+
+		#ifdef DEBUG
+		BOOST_LOG_TRIVIAL(info) << "Expr: " << expr;
+		BOOST_LOG_TRIVIAL(info) << "Annotated: " << annotated;
+		#endif // DEBUG
+	}
+
+	TEST_CASE("no bitvector ctes: case 1") {
+		const char* sample = "all x x = 1 && y = 1";
+		auto src = make_tau_source(sample, { .start = tau_parser::wff });
+		auto expr = make_statement(src);
+		auto annotated = annotate_bv_ctes(expr);
+		CHECK( select_all(expr, is_non_terminal<tau_parser::bv_type, Bool>).size() == 0 );
+		CHECK( select_all(annotated, is_non_terminal<tau_parser::bv_type, Bool>).size() == 0 );
+		CHECK( annotated == expr );
+
+		#ifdef DEBUG
+		BOOST_LOG_TRIVIAL(info) << "Expr: " << expr;
+		BOOST_LOG_TRIVIAL(info) << "Annotated: " << annotated;
+		#endif // DEBUG
+	}
+
+	TEST_CASE("simple free bitvector var") {
+		const char* sample = "x:bv16 =_ 1";
+		auto src = make_tau_source(sample, { .start = tau_parser::wff });
+		auto expr = make_statement(src);
+		auto annotated = annotate_bv_ctes(expr);
+		CHECK( select_all(expr, is_non_terminal<tau_parser::bv_type, Bool>).size() == 1 );
+		CHECK( select_all(annotated, is_non_terminal<tau_parser::bv_type, Bool>).size() == 2 );
+
+		#ifdef DEBUG
+		BOOST_LOG_TRIVIAL(info) << "Expr: " << expr;
+		BOOST_LOG_TRIVIAL(info) << "Annotated: " << annotated;
+		#endif // DEBUG
+	}
+
+	TEST_CASE("simple two bitvector ctes") {
+		const char* sample = "1:bv16 =_ 1";
+		auto src = make_tau_source(sample, { .start = tau_parser::wff });
+		auto expr = make_statement(src);
+		auto annotated = annotate_bv_ctes(expr);
+		CHECK( select_all(expr, is_non_terminal<tau_parser::bv_type, Bool>).size() == 1 );
+		CHECK( select_all(annotated, is_non_terminal<tau_parser::bv_type, Bool>).size() == 2 );
+
+		#ifdef DEBUG
+		BOOST_LOG_TRIVIAL(info) << "Expr: " << expr;
+		BOOST_LOG_TRIVIAL(info) << "Annotated: " << annotated;
+		#endif // DEBUG
+	}
+
+	TEST_CASE("simple bounded bitvector var") {
+		const char* sample = "all x:bv16 x:bv16 =_ 1";
+		auto src = make_tau_source(sample, { .start = tau_parser::wff });
+		auto expr = make_statement(src);
+		auto annotated = annotate_bv_ctes(expr);
+		CHECK( select_all(expr, is_non_terminal<tau_parser::bv_type, Bool>).size() == 2 );
+		CHECK( select_all(annotated, is_non_terminal<tau_parser::bv_type, Bool>).size() == 3 );
+
+		#ifdef DEBUG
+		BOOST_LOG_TRIVIAL(info) << "Expr: " << expr;
+		BOOST_LOG_TRIVIAL(info) << "Annotated: " << annotated;
+		#endif // DEBUG
+	}
+
 }
