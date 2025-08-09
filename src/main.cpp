7b0a6f0b8af73f8b4143cf9dbdddfdf66f382589
tklip <tomas@idni.org>
2025-08-09 12:43:39 +0200

    added experimental `tau -x <fname>`: refactored `run_tau_spec()` w/o using `repl_evaluator` and with multiline support

--- src/main.cpp
+++ src/main.cpp
@@ -97,8 +97,133 @@ cli::options tau_options() {
 
 int error(const string& s) { TAU_LOG_ERROR << "" << s; return 1; }
 
-int run_tau_spec(string spec_file, bool charvar, bool exp,
-	boost::log::trivial::severity_level sev) {
+// parse tau spec from a string using line by line parsing attempts
+// If a parse result is "Unexpected end of file", it appends next line and repeats.
+// This is used for making the dot ('.') at the end of a statement optional at
+// the end of a line.
+std::optional<rr<node<tau_ba<sbf_ba>, sbf_ba>>> get_spec_multiline(
+	const string& src)
+{
+	using node = tau_lang::node<tau_ba<sbf_ba>, sbf_ba>;
+	using tau = tree<node>;
+	std::istringstream iss(src);
+	std::string attempt, line, msg;
+	tref main = nullptr;
+	definitions<node>& defs = definitions<node>::instance();
+
+	while (std::getline(iss, line)) {
+		if (!attempt.empty()) attempt += "\n";
+		attempt += line;
+
+		auto result = tau_parser::instance().parse(
+			attempt.c_str(), attempt.size(),
+			{ .start = tau_parser::spec_multiline });
+		if (result.found) {
+			tref n = tau::get(result);
+			if (!n) return {};
+			const auto& t = tau::get(n);
+			if (t.child_is(tau::main)) {
+				if (main) {
+					msg = "Multiple main formulas";
+					break;
+				}
+				main = tau::trim2(n);
+			}
+			else if (t.child_is(tau::rec_relation))
+				defs.add(tau::geth(t[0][0]),
+					tau::geth(resolve_io_vars<node>(
+						defs.get_io_context(),
+						t[0].second())));
+			else if (t.child_is(tau::input_def)) {
+				if (!get_io_def<node>(t.first(),
+						      defs.get_input_defs()))
+				{
+					TAU_LOG_ERROR << "Invalid type "
+						<< TAU_TO_STR(t.first());
+					return {};
+				}
+			}
+			else if (t.child_is(tau::output_def)) {
+				if (!get_io_def<node>(t.first(),
+						      defs.get_output_defs()))
+				{
+					TAU_LOG_ERROR << "Invalid type "
+						<< TAU_TO_STR(t.first());
+					return {};
+				}
+			}
+			attempt = "";
+		} else {
+			// Check if the error contains "Unexpected end of file"
+			msg = result.parse_error.to_str(
+				tau_parser::error::info_lvl::INFO_BASIC);
+			// if error is not "Unexpected end of file", break to log it and return fail
+			if (msg.find("Unexpected end of file")
+						== std::string::npos) break;
+		}
+	}
+	if (msg.empty() && !main) msg = "No main formula";
+	if (msg.size()) {
+		TAU_LOG_ERROR << "[tau] " << msg << "\n";
+		return {};
+	}
+	rr<node> nso_rr{ tau::geth(resolve_io_vars<node>(
+		defs.get_io_context(), main)) };
+	auto definitions = defs.get_sym_defs();
+	for (auto& def : definitions) nso_rr.rec_relations.emplace_back(
+		def.first,
+		tau::geth(resolve_io_vars<node>(
+			defs.get_io_context(), def.second->get())));
+	if (auto infr = infer_ref_types<node>(nso_rr); infr)
+		return infr.value();
+	return {};
+}
+
+int run_tau_spec(string spec_file, bool charvar, bool /*exp*/,
+	boost::log::trivial::severity_level sev)
+{
+	logging::set_filter(sev);
+
+	string src = "";
+	if (spec_file == "-") {
+		std::ostringstream oss;
+		oss << std::cin.rdbuf(), src = oss.str();
+	} else {
+		TAU_LOG_TRACE << "open file: " << spec_file;
+		std::ifstream ifs(spec_file, std::ios::binary | std::ios::ate);
+		if (!ifs) return error("Cannot open file " + spec_file);
+		auto l = ifs.tellg();
+		src.resize(l), ifs.seekg(0), ifs.read(&src[0], l);
+	}
+	if (src.empty()) return 0;
+	
+	using node = tau_lang::node<tau_ba<sbf_ba>, sbf_ba>;
+	
+	std::set<std::string> guards{ charvar ? "charvar" : "var" };
+	tau_parser::instance().get_grammar().set_enabled_productions(guards);
+	sbf_parser::instance().get_grammar().set_enabled_productions(guards);
+
+	auto spec_opt = get_spec_multiline(src);
+	if (!spec_opt) return 1;
+	tref applied = apply_rr_to_formula(spec_opt.value());
+	if (!applied) return 2;
+	tref normalized = normalizer<node>(applied);
+	if (!normalized) return 3;
+	if (has_free_vars<node>(normalized)) return 4;
+	definitions<node>& defs = definitions<node>::instance();
+	auto ins = finputs<node>(
+		collect_input_streams<node>(normalized, defs.get_io_context()));
+	auto outs = foutputs<node>(
+		collect_output_streams<node>(normalized,defs.get_io_context()));
+	auto result = run<node>(normalized, ins, outs, defs.get_io_context());
+	if (!result) return 5;
+	return 0;
+}
+
+// TODO (LOW) after new run_tau_spec is stable, remove this function
+int run_tau_spec_old(string spec_file, bool charvar, bool exp,
+	boost::log::trivial::severity_level sev)
+{
 	string src = "";
 	if (spec_file == "-") {
 		std::ostringstream oss;
@@ -170,7 +295,10 @@ int main(int argc, char** argv) {
 	bool exp = opts["experimental"].get<bool>();
 
 	// spec provided, run it
-	if (files.size()) return run_tau_spec(files.front(), charvar, exp, sev);
+	if (files.size()) {
+		if (exp) return run_tau_spec(files.front(), charvar, exp, sev);
+		else return run_tau_spec_old(files.front(), charvar, exp, sev);
+	}
 
 	repl_evaluator<sbf_ba> re({
 		.status = opts["status"].get<bool>(),
