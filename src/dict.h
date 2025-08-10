975f5210de181fc2107861793468308df697162d
tklip <tomas@idni.org>
2025-08-10 16:21:50 +0200

    ensure dict initialization

--- src/dict.h
+++ src/dict.h
@@ -12,17 +12,20 @@
 
 namespace idni::tau_lang {
 
-inline static std::vector<std::string> S{ "" };                // strings
-inline static std::map<std::string, size_t> SM{ { "", 0 } };   // string -> id map
+inline std::vector<std::string>& S() {
+	static std::vector<std::string> S{ "" };                // strings
+	return S;
+}
 
 inline size_t dict(const std::string& s) {
+	static std::map<std::string, size_t> SM{ { "", 0 } };   // string -> id map
 	if (auto it = SM.find(s); it != SM.end()) return it->second;
-	return SM.emplace(s, S.size()), S.push_back(s), S.size() - 1;
+	return SM.emplace(s, S().size()), S().push_back(s), S().size() - 1;
 }
 
 inline const std::string& dict(size_t id) {
-	DBG(assert(id <= S.size());)
-	return S[id];
+	DBG(assert(id <= S().size());)
+	return S()[id];
 }
 
 } // namespace idni::tau_lang
