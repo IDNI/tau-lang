dd5643db1a707e47bf5ddd5513fc58ad2dc2d100
castrod <david.castroesteban@gmail.com>
2025-08-27 17:57:42 +0200

    adding sat bv tests

---
+++ tests/integration/test_integration-satisfiability4.cpp
@@ -0,0 +1,36 @@
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.txt
+
+#include "test_integration-satisfiability_helper.h"
+
+TEST_SUITE("Alignments y4") {
+	// TODO (HIGH) fails in cli (returns T)
+	TEST_CASE("greater_lookback_two_st_1") {  // <- takes too long
+		tref spec = create_spec("(always o1[t]:bv =_ 1 && o2[t]:bv =_ 1) && (sometimes o1[t-1]:bv =_ 1) && (sometimes o2[t-2]:bv =_ 0).");
+		CHECK(!is_tau_formula_sat<node_t>(spec));
+	}
+	// TODO (HIGH) fails in cli (returns T)
+	TEST_CASE("greater_lookback_two_st_2") {
+		tref spec = create_spec("(always o1[t]:bv =_ 1 && o2[t]:bv =_ 1) && (sometimes o1[t-1]:bv =_ 0) && (sometimes o2[t-2]:bv =_ 1).");
+		CHECK(!is_tau_formula_sat<node_t>(spec));
+	}
+	// TODO (HIGH) fails in cli (returns T)
+	TEST_CASE("smaller_lookback_two_st_1") {
+		tref spec = create_spec("(always o1[t-2]:bv =_ 0 && o2[t-2]:bv =_ 0) && (sometimes o1[t]:bv =_ 1) && (sometimes o1[t-1]:bv =_ 0).");
+		CHECK(!is_tau_formula_sat<node_t>(spec));
+	}
+	// TODO (HIGH) fails in cli (returns T)
+	TEST_CASE("smaller_lookback_two_st_2") {
+		tref spec = create_spec("(always o1[t-2]:bv =_ 0 && o2[t-2]:bv =_ 0) && (sometimes o1[t]:bv =_ 0) && (sometimes o1[t-1]:bv =_ 1).");
+		CHECK(!is_tau_formula_sat<node_t>(spec));
+	}
+	// TODO (HIGH) fails in cli (returns T)
+	TEST_CASE("mixed_lookback_two_st_1") {
+		tref spec = create_spec("(always o1[t-2]:bv =_ 1) && (sometimes o1[t-3]:bv =_ 0) && (sometimes o1[t]:bv =_ 1).");
+		CHECK(!is_tau_formula_sat<node_t>(spec));
+	}
+	// TODO (HIGH) fails in cli (returns T)
+	TEST_CASE("mixed_lookback_two_st_2") {
+		tref spec = create_spec("(always o1[t-2]:bv =_ 1) && (sometimes o1[t-3]:bv =_ 1) && (sometimes o1[t]:bv =_ 0).");
+		CHECK(!is_tau_formula_sat<node_t>(spec));
+	}
+}
