a83cda3a5ea99761f5735cd7658b76a2d84e31f3
castrod <david.castroesteban@gmail.com>
2025-09-15 10:34:21 +0200

    adding test cases for merge_ba_types

--- tests/unit/test_resolver.cpp
+++ tests/unit/test_resolver.cpp
@@ -120,3 +120,48 @@ TEST_SUITE("scopes") {
 		CHECK(kinds_map[3] == 3);
 	}
 }
+
+TEST_SUITE("merge_ba_types") {
+
+	bool match(const type& type1, const type& type2, const type& result) {
+		auto merged = merge_ba_types<node_t>(type1, type2);
+		if (!merged) return false;
+		return (result.first == merged.value().first
+			&& result.second == merged.value().second);
+	}
+
+	bool nomatch(const type& type1, const type& type2) {
+		auto merged = merge_ba_types<node_t>(type1, type2);
+		return !merged.has_value();
+	}
+
+	TEST_CASE("untyped with untyped") {
+		CHECK(match({0, nullptr}, {0, nullptr}, {0, nullptr}));
+		CHECK(match({0, (tref)1}, {0, nullptr}, {0, nullptr}));
+		CHECK(match({0, nullptr}, {0, (tref)1}, {0, nullptr}));
+	}
+
+	TEST_CASE("untyped with any type") {
+		CHECK(match({0, nullptr}, {1, nullptr}, {1, nullptr}));
+		CHECK(match({0, nullptr}, {1, (tref)1}, {1, (tref)1}));
+		CHECK(match({1, nullptr}, {0, nullptr}, {1, nullptr}));
+		CHECK(match({1, (tref)1}, {0, nullptr}, {1, (tref)1}));
+		CHECK(match({0, nullptr}, {0, nullptr}, {0, nullptr}));
+	}
+
+	TEST_CASE("same types all possible subtypes") {
+		CHECK(match({1, nullptr}, {1, nullptr}, {1, nullptr}));
+		CHECK(match({1, (tref)1}, {1, (tref)1}, {1, (tref)1}));
+		CHECK(match({1, nullptr}, {1, (tref)1}, {1, (tref)1}));
+		CHECK(match({1, (tref)1}, {1, nullptr}, {1, (tref)1}));
+	}
+
+	TEST_CASE("different types") {
+		CHECK(nomatch({1, nullptr}, {2, nullptr}));
+		CHECK(nomatch({1, (tref)1}, {1, (tref)2}));
+	}
+}
+
+TEST_SUITE("merge_ba_types") {}
+
+TEST_SUITE("new_infer_ba_types") {}
--- tests/unit/test_union_find.cpp
+++ tests/unit/test_union_find.cpp
@@ -1,92 +1,90 @@
 // To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.txt
 
-
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
+#include "test_init.h"
 #include "union_find.h"
 
 using namespace idni::tau_lang;
 
 TEST_SUITE("union_find") {
 
-TEST_CASE("insert and contains") {
-	union_find<int> uf;
-	uf.insert(1);
-	uf.insert(2);
-	uf.insert(3);
-	CHECK(uf.contains(1));
-	CHECK(uf.contains(2));
-	CHECK(uf.contains(3));
-	CHECK(!uf.contains(4));
-	CHECK(uf.size() == 3);
-}
-
-TEST_CASE("merge and connected") {
-	union_find<int> uf;
-	uf.insert(1);
-	uf.insert(2);
-	uf.insert(3);
-	uf.merge(1, 2);
-	CHECK(uf.connected(1, 2));
-	CHECK(!uf.connected(1, 3));
-	uf.merge(2, 3);
-	CHECK(uf.connected(1, 3));
-}
-
-TEST_CASE("root and path compression") {
-	union_find<int> uf;
-	uf.insert(1);
-	uf.insert(2);
-	uf.insert(3);
-	uf.merge(1, 2);
-	uf.merge(2, 3);
-	int r1 = uf.root(1);
-	int r2 = uf.root(2);
-	int r3 = uf.root(3);
-	CHECK(r1 == r2);
-	CHECK(r2 == r3);
-}
-
-TEST_CASE("get_sets returns correct sets") {
-	union_find<int> uf;
-	uf.insert(1);
-	uf.insert(2);
-	uf.insert(3);
-	uf.insert(4);
-	uf.merge(1, 2);
-	uf.merge(3, 4);
-	auto sets = uf.get_sets();
-	CHECK(sets.size() == 2);
-	bool found12 = false, found34 = false;
-	for (const auto& [root, members] : sets) {
-		if (members.contains(1) && members.contains(2)) found12 = true;
-		if (members.contains(3) && members.contains(4)) found34 = true;
+	TEST_CASE("insert and contains") {
+		union_find<int> uf;
+		uf.insert(1);
+		uf.insert(2);
+		uf.insert(3);
+		CHECK(uf.contains(1));
+		CHECK(uf.contains(2));
+		CHECK(uf.contains(3));
+		CHECK(!uf.contains(4));
+		CHECK(uf.size() == 3);
 	}
-	CHECK(found12);
-	CHECK(found34);
-}
 
-TEST_CASE("size after merges and inserts") {
-	union_find<int> uf;
-	uf.insert(1);
-	uf.insert(2);
-	uf.insert(3);
-	CHECK(uf.size() == 3);
-	uf.merge(1, 2);
-	CHECK(uf.size() == 3); // size does not decrease
-	uf.insert(4);
-	CHECK(uf.size() == 4);
-}
+	TEST_CASE("merge and connected") {
+		union_find<int> uf;
+		uf.insert(1);
+		uf.insert(2);
+		uf.insert(3);
+		uf.merge(1, 2);
+		CHECK(uf.connected(1, 2));
+		CHECK(!uf.connected(1, 3));
+		uf.merge(2, 3);
+		CHECK(uf.connected(1, 3));
+	}
 
-TEST_CASE("connected for non-inserted elements") {
-	union_find<int> uf;
-	CHECK(!uf.connected(1, 2));
-	uf.insert(1);
-	CHECK(!uf.connected(1, 2));
-	uf.insert(2);
-	CHECK(!uf.connected(1, 2));
-	uf.merge(1, 2);
-	CHECK(uf.connected(1, 2));
-}
+	TEST_CASE("root and path compression") {
+		union_find<int> uf;
+		uf.insert(1);
+		uf.insert(2);
+		uf.insert(3);
+		uf.merge(1, 2);
+		uf.merge(2, 3);
+		int r1 = uf.root(1);
+		int r2 = uf.root(2);
+		int r3 = uf.root(3);
+		CHECK(r1 == r2);
+		CHECK(r2 == r3);
+	}
+
+	TEST_CASE("get_sets returns correct sets") {
+		union_find<int> uf;
+		uf.insert(1);
+		uf.insert(2);
+		uf.insert(3);
+		uf.insert(4);
+		uf.merge(1, 2);
+		uf.merge(3, 4);
+		auto sets = uf.get_sets();
+		CHECK(sets.size() == 2);
+		bool found12 = false, found34 = false;
+		for (const auto& [root, members] : sets) {
+			if (members.contains(1) && members.contains(2)) found12 = true;
+			if (members.contains(3) && members.contains(4)) found34 = true;
+		}
+		CHECK(found12);
+		CHECK(found34);
+	}
+
+	TEST_CASE("size after merges and inserts") {
+		union_find<int> uf;
+		uf.insert(1);
+		uf.insert(2);
+		uf.insert(3);
+		CHECK(uf.size() == 3);
+		uf.merge(1, 2);
+		CHECK(uf.size() == 3); // size does not decrease
+		uf.insert(4);
+		CHECK(uf.size() == 4);
+	}
+
+	TEST_CASE("connected for non-inserted elements") {
+		union_find<int> uf;
+		CHECK(!uf.connected(1, 2));
+		uf.insert(1);
+		CHECK(!uf.connected(1, 2));
+		uf.insert(2);
+		CHECK(!uf.connected(1, 2));
+		uf.merge(1, 2);
+		CHECK(uf.connected(1, 2));
+	}
 
 }
