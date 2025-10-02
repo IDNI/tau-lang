25743b94fe8a557f867257b5e57428a45c2a5544
Lucca Tiemens <lucca@tiemens.de>
2025-10-02 12:14:47 +0200

    Updated tests concerning new ordering.

--- tests/integration/test_integration-tau_ba3.cpp
+++ tests/integration/test_integration-tau_ba3.cpp
@@ -25,11 +25,11 @@ TEST_SUITE("allowing unresolved rr's in normalization") {
 
 	TEST_CASE("ex x f[0](x) && x = 0.") {
 		const char* sample = "ex x f[0](x) && x = 0.";
-		CHECK( normalize_and_check(sample, "ex x f[0](x) && x = 0") );
+		CHECK( normalize_and_check(sample, "ex x x = 0 && f[0](x)") );
 	}
 
 	TEST_CASE("ex x f[0](x) && x != 0.") {
 		const char* sample = "ex x f[0](x) && x != 0.";
-		CHECK( normalize_and_check(sample, "ex x f[0](x) && x != 0") );
+		CHECK( normalize_and_check(sample, "ex x x != 0 && f[0](x)") );
 	}
 }
