0cf5eb88cbe9b4a55073832720dc7a5039b3711a
castrod <david.castroesteban@gmail.com>
2025-10-09 11:34:34 +0200

    removing old ba_types_inference

--- tests/unit/test_ba_types_inference.cpp
+++ tests/unit/test_ba_types_inference.cpp
@@ -3,56 +3,327 @@
 #include "test_init.h"
 #include "test_Bool_helpers.h"
 
-tref infer(const char* sample) {
-	tref code = tau::get(sample);
-	DBG(assert(code != nullptr);)
-	return infer_ba_types<node_t>(code);
+
+#include "ba_types_inference.h"
+
+// Adding comprehensive doctest test cases for scope in resolver.h
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+using namespace idni::tau_lang;
+
+TEST_SUITE("Configuration") {
+	TEST_CASE("logging") {
+		logging::trace();
+	}
 }
 
-bool expect_infer_fail(const char* sample) {
-	return infer(sample) == nullptr;
+TEST_SUITE("scopes") {
+    TEST_CASE("empty scope is empty") {
+		scoped_resolver<size_t, size_t> r(0);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 0);
+	}
+
+	TEST_CASE("closing outer scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		r.close();
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 0);
+	}
+
+	TEST_CASE("opening inner scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		r.open({});
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 1);
+	}
+
+	TEST_CASE("opening clossing scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		r.open({});
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 1);
+		r.close();
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 1);
+	}
+
+	TEST_CASE("opening two nested inner scopes") {
+		scoped_resolver<size_t, size_t> r(0);
+		r.open({});
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 1);
+		r.open({});
+		CHECK(r.scopes_.size() == 3);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 2);
+	}
+
+	TEST_CASE("closing two nested inner scopes") {
+		scoped_resolver<size_t, size_t> r(0);
+		r.open({});
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 1);
+		r.open({});
+		CHECK(r.scopes_.size() == 3);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 2);
+		r.close();
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 2);
+		r.close();
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 2);
+	}
+
+	TEST_CASE("opening/closing two sibling inner scopes") {
+		scoped_resolver<size_t, size_t> r(0);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 0);
+		r.open({});
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 1);
+		r.close();
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 1);
+		r.open({});
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 2);
+		r.close();
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.kinds_.size() == 0);
+		CHECK(r.current == 2);
+	}
+
+    TEST_CASE("adding unkinded element to empty scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		r.insert(x);
+		CHECK(r.kinds().size() == 1);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 1);
+		CHECK(r.type_of(x) == r.unknown);
+	}
+
+    TEST_CASE("assigning to non-existing element in empty scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t type = 1;
+		CHECK(!r.assign(x, type));
+		CHECK(r.kinds().size() == 0);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 0);
+	}
+
+    TEST_CASE("adding and assign element to empty scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t type = 1;
+		r.insert(x);
+		r.assign(x, type);
+		CHECK(r.type_of(x) == type);
+		CHECK(r.kinds().size() == 1);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 1);
+	}
+
+	TEST_CASE("assigning unkinded element to a non-empty scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t y = 1;
+		r.insert(x);
+		r.insert(y);
+		CHECK(r.kinds().size() == 2);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == r.unknown);
+		CHECK(r.type_of(y) == r.unknown);
+	}
+
+	TEST_CASE("assigning kinded element to a non-empty scope: existing unkinded") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t y = 1;
+		size_t type = 2;
+		r.insert(x);
+		r.insert(y);
+		CHECK(r.assign(y, type));
+		CHECK(r.kinds().size() == 2);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == r.unknown);
+		CHECK(r.type_of(y) == type);
+	}
+
+	TEST_CASE("assigning kinded element to a non-empty scope: existing kinded") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t y = 1;
+		size_t type1 = 2;
+		size_t type2 = 3;
+		r.insert(x);
+		r.insert(y);
+		CHECK(r.assign(y, type1));
+		CHECK(r.assign(x, type2)); // cannot assign conflicting kind
+		CHECK(r.kinds().size() == 2);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == type2);
+		CHECK(r.type_of(y) == type1);
+	}
+
+	TEST_CASE("assigning unkinded element to a nested scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t y = 1;
+		r.insert(x);
+		r.open({});
+		r.insert(y);
+		CHECK(r.kinds().size() == 2);
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == r.unknown);
+		CHECK(r.type_of(y) == r.unknown);
+	}
+
+	TEST_CASE("assigning kinded element to a nested scope (y1)") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t y = 1;
+		size_t type = 2;
+		r.insert(x);
+		r.open({});
+		r.insert(y);
+		CHECK(r.assign(y, type));
+		CHECK(r.kinds().size() == 2);
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == r.unknown);
+		CHECK(r.type_of(y) == type);
+	}
+
+	TEST_CASE("assigning kinded element to a nested scope (y2)") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t y = 1;
+		size_t type = 2;
+		r.insert(x);
+		CHECK(r.assign(x, type));
+		r.open({});
+		r.insert(y);
+		CHECK(r.kinds().size() == 2);
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == type);
+		CHECK(r.type_of(y) == r.unknown);
+	}
+
+	TEST_CASE("overwriting kinded element kind in a nested scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t type1 = 2;
+		size_t type2 = 3;
+		r.insert(x);
+		CHECK(r.assign(x, type1));
+		r.open({{x, type2}});
+		CHECK(r.kinds().size() == 1);
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == type2);
+		r.close();
+		CHECK(r.kinds().size() == 1);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == type1);
+	}
+
+	TEST_CASE("overwriting unkinded element kind in a nested scope") {
+		scoped_resolver<size_t, size_t> r(0);
+		size_t x = 0;
+		size_t type1 = 2;
+		r.insert(x);
+		r.open({{x, type1}});
+		CHECK(r.kinds().size() == 1);
+		CHECK(r.scopes_.size() == 2);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == type1);
+		r.close();
+		CHECK(r.kinds().size() == 1);
+		CHECK(r.scopes_.size() == 1);
+		CHECK(r.kinds_.size() == 2);
+		CHECK(r.type_of(x) == r.unknown);
+	}
 }
 
-bool are_all_typed_as(tref n, const std::string& type) {
-	using node = node_t;
-	size_t tid = get_ba_type_id<node>(type);
-	auto are_all_trefs_typed = [&](const trefs& ts) {
-		for (tref t : ts)
-			if (tau::get(t).get_ba_type() != tid) {
-				TAU_LOG_ERROR << "Type mismatch: " << TAU_LOG_FM(t)
-					<< " expected: " << TAU_LOG_BA_TYPE(tid)
-					<< " got: " << TAU_LOG_BA_TYPE(tau::get(t)
-								.get_ba_type());
-				return false;
-			}
-		return true;
-	};
-	const auto& t = tau::get(n);
-	return are_all_trefs_typed(t.select_all(is<node, tau::bf_constant>))
-	    && are_all_trefs_typed(t.select_all(is<node, tau::variable>));
-}
+TEST_SUITE("merge_ba_types") {
 
-TEST_SUITE("constant types") {
-	TEST_CASE("all typed") {
-		auto n = infer("{ 0 } : bool = { 1 } : bool & { 0 } : bool.");
-		CHECK( are_all_typed_as(n, "bool") );
+	using type_t = tree<node_t>::type_t;
+
+	bool match(const type_t& type1, const type_t& type2, const type_t& result) {
+		auto merged = merge_ba_types<node_t>(type1, type2);
+		if (!merged) return false;
+		return (result.first == merged.value().first
+			&& result.second == merged.value().second);
 	}
 
-	TEST_CASE("some typed") {
-		using node = node_t;
-		auto n = infer("{ 0 } : bool = { 1 } { 0 } : bool.");
-		TAU_LOG_TRACE << "inferred: " << TAU_LOG_FM_DUMP(n);
-		CHECK( are_all_typed_as(n, "bool") );
+	bool nomatch(const type_t& type1, const type_t& type2) {
+		auto merged = merge_ba_types<node_t>(type1, type2);
+		return !merged.has_value();
 	}
 
-	TEST_CASE("only 1 typed") {
-		auto n = infer("{ 0 } = { 1 } & { 0 } : bool.");
-		CHECK( are_all_typed_as(n, "bool") );
+	TEST_CASE("untyped with untyped") {
+		CHECK(match({0, nullptr}, {0, nullptr}, {0, nullptr}));
+		CHECK(match({0, (tref)1}, {0, nullptr}, {0, nullptr}));
+		CHECK(match({0, nullptr}, {0, (tref)1}, {0, nullptr}));
 	}
 
-	TEST_CASE("none typed") {
-		auto n = infer("{ 0 } = { 1 } { 0 }.");
-		CHECK( are_all_typed_as(n, "bool") );
+	TEST_CASE("untyped with any type") {
+		CHECK(match({0, nullptr}, {1, nullptr}, {1, nullptr}));
+		CHECK(match({0, nullptr}, {1, (tref)1}, {1, (tref)1}));
+		CHECK(match({1, nullptr}, {0, nullptr}, {1, nullptr}));
+		CHECK(match({1, (tref)1}, {0, nullptr}, {1, (tref)1}));
+		CHECK(match({0, nullptr}, {0, nullptr}, {0, nullptr}));
 	}
 
+	TEST_CASE("same types all possible subtypes") {
+		CHECK(match({1, nullptr}, {1, nullptr}, {1, nullptr}));
+		CHECK(match({1, (tref)1}, {1, (tref)1}, {1, (tref)1}));
+		CHECK(match({1, nullptr}, {1, (tref)1}, {1, (tref)1}));
+		CHECK(match({1, (tref)1}, {1, nullptr}, {1, (tref)1}));
+	}
+
+	TEST_CASE("different types") {
+		CHECK(nomatch({1, nullptr}, {2, nullptr}));
+		CHECK(nomatch({1, (tref)1}, {1, (tref)2}));
+	}
 }
