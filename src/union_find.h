6663b64a3275e9668646fa867a14cd3b853b4fc3
castrod <david.castroesteban@gmail.com>
2025-10-09 12:35:42 +0200

    moving scoped_resolver to union_find as scoped_union_find (also in tests)

--- src/union_find.h
+++ src/union_find.h
@@ -3,6 +3,8 @@
 #ifndef __IDNI__TAU__UNION_FIND_H__
 #define __IDNI__TAU__UNION_FIND_H__
 
+#include <deque>
+
 namespace idni::tau_lang {
 
 /**
@@ -98,6 +100,111 @@ struct union_find_by_less : public union_find<data_t, less_t> {
 		}
 		this->operator[](root_x) = root_y;
 		return root_y;
+	}
+};
+
+
+template<typename data_t, typename kind_t, class less_t = std::less<data_t>>
+struct scoped_union_find {
+	using scope_t = size_t;
+	using element_t = std::pair<scope_t, data_t>;
+
+	struct scoped_less {
+		bool operator()(const element_t& a, const element_t& b) const {
+				static const less_t comp;
+				if (a.first < b.first) return true;
+				if (a.first > b.first) return false;
+				// same scope
+				return (comp(a.second, b.second));
+		}
+	};
+
+	using kinds_t = std::map<element_t, kind_t, scoped_less>;
+	using union_find_by_less_t = union_find_by_less<element_t, scoped_less>;
+
+	union_find_by_less_t uf;
+	scope_t current = 0;
+	std::deque<size_t> scopes_ { current };
+	kinds_t kinds_;
+	kind_t unknown;
+	kind_t default_kind = unknown;
+
+	scoped_union_find(const kind_t& unknown): unknown(unknown) {}
+
+	void open(const std::map<data_t, kind_t, less_t>& kinds) {
+		current++;
+		scopes_.push_back(current);
+		for (const auto& [data, kind] : kinds) {
+			uf.insert({current, data});
+			kinds_.emplace(element_t{current, data}, kind);
+		}
+	}
+
+	void close() {
+		if (scopes_.size() == 1) return;
+		/*std::erase_if(kinds_, [&](const auto& e)
+			{ return e.first.first == current; });*/
+		scopes_.pop_back();
+	}
+
+	void insert(const data_t& data) {
+		static size_t global = 0;
+		for(auto it = scopes_.rbegin(); it != scopes_.rend(); ++it)
+			if (uf.contains(element_t{*it, data})) return;
+		uf.insert({global, data});
+		kinds_.emplace(element_t{global, data}, unknown);
+	}
+
+	kind_t type_of(const data_t& data) {
+		for(auto it = scopes_.rbegin(); it != scopes_.rend(); ++it)
+			if (uf.contains({*it, data}))
+				return kinds_.find(uf.root(element_t{*it, data}))->second;
+		return unknown;
+	}
+
+	scope_t scope_of(const data_t& data) {
+		for(auto it = scopes_.rbegin(); it != scopes_.rend(); ++it)
+			if (uf.contains({*it, data}))
+				return (*it);
+		return 0; // global scope
+	}
+
+	bool same_kind(const data_t& data1, const data_t& data2) {
+		return type_of(data1) == type_of(data2);
+	}
+
+	bool assign(const data_t& data, const kind_t& kind) {
+		for(auto it = scopes_.rbegin(); it != scopes_.rend(); ++it)
+			if (uf.contains({*it, data})) {
+				if (auto current = kinds_.find(uf.root(element_t{*it, data}))->second;
+						current != unknown && current != kind)
+					return false;
+				kinds_.insert_or_assign(uf.root(element_t{*it, data}), kind);
+				return true;
+			}
+		return false;
+	}
+
+	bool assign(const data_t& data) {
+		return assign(data, default_kind);
+	}
+
+	std::map<data_t, kind_t, less_t> kinds() {
+		std::map<data_t, kind_t, less_t> result ;
+		// Note that we will overwrite the kind of a data if it appears in
+		// multiple scopes, keeping only the inner one.
+		for(auto it = uf.begin(); it != uf.end(); ++it)
+			if (kinds_.find(it->second) != kinds_.end())
+				result[it->second.second] = type_of(it->second.second);
+		return result;
+	}
+
+	std::map<data_t, kind_t, less_t> current_kinds() {
+		std::map<data_t, kind_t, less_t> result ;
+		auto scope = scopes_.back();
+		for(auto [element, _] : uf)
+			if (element.first == scope) result[element.second] = type_of(element.second);
+		return result;
 	}
 };
 
