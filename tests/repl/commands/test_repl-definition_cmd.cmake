c87a8c350eeef5401d6a6f523470c5ffdcb5c6ff
castrod <david.castroesteban@gmail.com>
2025-12-09 07:20:18 +0100

    updating tau syntax in repl test cases

--- tests/repl/commands/test_repl-definition_cmd.cmake
+++ tests/repl/commands/test_repl-definition_cmd.cmake
@@ -8,7 +8,7 @@ include(add_repl_test)
 add_repl_test(def_wff_cmd "g(Y) := T" "\\[1\\] g")
 
 # definition command rec_relation
-add_repl_test(def_bf_cmd "g(Y) := 1" "\\[1\\] g")
+add_repl_test(def_bf_cmd "g(Y):tau := 1" "\\[1\\] g")
 
 # definition command list
 #
@@ -16,7 +16,7 @@ add_repl_test(def_bf_cmd "g(Y) := 1" "\\[1\\] g")
 add_repl_test(def_list_cmd "defs" "Definitions: empty")
 
 # with defs
-add_repl_test(def_list_cmd-with_defs "g(Y) := 1. defs" "\\[1\\] g")
+add_repl_test(def_list_cmd-with_defs "g(Y):tau := 1. defs" "\\[1\\] g")
 
 # definition command print
 #
@@ -24,9 +24,9 @@ add_repl_test(def_list_cmd-with_defs "g(Y) := 1. defs" "\\[1\\] g")
 add_repl_test(def_print_cmd-empty "defs" "Definitions: empty")
 
 # printing defs
-add_repl_test(def_print_cmd-1 "g(Y) := 1. defs 1" "\\[1\\] g")
-add_repl_test(def_print_cmd-2 "g(Y) := 1. f(Y) := T. defs 2" "\\[2\\] f")
+add_repl_test(def_print_cmd-1 "g(Y):tau := 1. defs 1" "\\[1\\] g")
+add_repl_test(def_print_cmd-2 "g(Y):tau := 1. f(Y) := T. defs 2" "\\[2\\] f")
 
 # not existing defs
-add_repl_test_fail(def_print_cmd-0 "g(Y) := 1. defs 0" "Definition \\[0\\] does not exist")
-add_repl_test_fail(def_print_cmd-3 "g(Y) := 1. defs 3" "Definition \\[3\\] does not exist")
+add_repl_test_fail(def_print_cmd-0 "g(Y):tau := 1. defs 0" "Definition \\[0\\] does not exist")
+add_repl_test_fail(def_print_cmd-3 "g(Y):tau := 1. defs 3" "Definition \\[3\\] does not exist")
