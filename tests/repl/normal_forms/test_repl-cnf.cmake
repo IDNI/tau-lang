f8d61a2ce08aebcf95cfcdc5cc814ffaabfbf057
Lucca Tiemens <lucca@tiemens.de>
2026-01-05 16:37:38 +0100

    Introduced printing of '&' before parenthesis.

--- tests/repl/normal_forms/test_repl-cnf.cmake
+++ tests/repl/normal_forms/test_repl-cnf.cmake
@@ -17,5 +17,5 @@ add_repl_test(normal_forms-cnf_wff_dist_left "cnf ((X=0 && Y=0) || Z=0)" ": \\(X
 
 add_repl_test(normal_forms-cnf_bf_1 "cnf 1" ": 1")
 add_repl_test(normal_forms-cnf_bf_0 "cnf 0" ": 0")
-add_repl_test(normal_forms-cnf_bf_dist_right "cnf (X | (Y & Z))" ": \\(.\\|.\\)\\(.\\|.\\)|")
-add_repl_test(normal_forms-cnf_bf_dist_left "cnf ((X & Y) | Z)" ": \\(.\\|.\\)\\(.\\|.\\)")
+add_repl_test(normal_forms-cnf_bf_dist_right "cnf (X | (Y & Z))" ": \\(.\\|.\\)&\\(.\\|.\\)|")
+add_repl_test(normal_forms-cnf_bf_dist_left "cnf ((X & Y) | Z)" ": \\(.\\|.\\)&\\(.\\|.\\)")
