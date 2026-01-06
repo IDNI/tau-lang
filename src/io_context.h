3ff51ff12496809843732f0a7263d3d754c64860
tklip <tomas@idni.org>
2026-01-06 14:47:16 +0100

    `io_context` as an enhanced replacement for spec_context

---
+++ src/io_context.h
@@ -0,0 +1,111 @@
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.txt
+
+#ifndef __IDNI__TAU__IO_CONTEXT_H__
+#define __IDNI__TAU__IO_CONTEXT_H__
+
+#include "tau_tree.h"
+
+namespace idni::tau_lang {
+
+struct serialized_constant_input_stream {
+	virtual ~serialized_constant_input_stream() = default;
+	virtual std::shared_ptr<serialized_constant_input_stream> rebuild() = 0;
+	virtual std::optional<std::string> get() = 0;
+};
+
+struct serialized_constant_output_stream {
+	virtual ~serialized_constant_output_stream() = default;
+	virtual std::shared_ptr<serialized_constant_output_stream> rebuild() = 0;
+	virtual bool put(const std::string& value) = 0;
+};
+
+template <NodeType node>
+using input_streams = subtree_map<node,
+	std::shared_ptr<serialized_constant_input_stream>>;
+template <NodeType node>
+using output_streams = subtree_map<node,
+	std::shared_ptr<serialized_constant_output_stream>>;
+using input_streams_remap  = std::map<std::string,
+	std::shared_ptr<serialized_constant_input_stream>>;
+using output_streams_remap = std::map<std::string,
+	std::shared_ptr<serialized_constant_output_stream>>;
+
+struct console_input_stream : public serialized_constant_input_stream {
+	virtual ~console_input_stream() = default;
+	virtual std::shared_ptr<serialized_constant_input_stream> rebuild() override;
+	virtual std::optional<std::string> get() override;
+};
+
+struct console_output_stream : public serialized_constant_output_stream {
+	virtual ~console_output_stream() = default;
+	virtual std::shared_ptr<serialized_constant_output_stream> rebuild() override;
+	virtual bool put(const std::string& value) override;
+};
+
+struct file_input_stream : public serialized_constant_input_stream {
+	file_input_stream(const std::string& filename);
+	virtual ~file_input_stream();
+	virtual std::shared_ptr<serialized_constant_input_stream> rebuild() override;
+	virtual std::optional<std::string> get() override;
+protected:
+	const std::string filename;
+	std::ifstream file;
+};
+
+struct file_output_stream : public serialized_constant_output_stream {
+	file_output_stream(const std::string& filename);
+	virtual ~file_output_stream();
+	virtual std::shared_ptr<serialized_constant_output_stream> rebuild() override;
+	virtual bool put(const std::string& value) override;
+protected:
+	const std::string filename;
+	std::ofstream file;
+};
+
+struct vector_input_stream : public serialized_constant_input_stream {
+	vector_input_stream(const std::vector<std::string>& values);
+	virtual ~vector_input_stream() = default;
+	virtual std::shared_ptr<serialized_constant_input_stream> rebuild() override;
+	virtual std::optional<std::string> get() override;
+protected:
+	std::vector<std::string> values;
+	size_t current = 0;
+};
+
+struct vector_output_stream : public serialized_constant_output_stream {
+	vector_output_stream();
+	virtual ~vector_output_stream() = default;
+	virtual std::shared_ptr<serialized_constant_output_stream> rebuild() override;
+	virtual bool put(const std::string& value) override;
+	std::vector<std::string> get_values() const;
+protected:
+	std::vector<std::string> values{};
+};
+
+// context for input and output streams and their types
+template <NodeType node>
+struct io_context {
+	subtree_map<node, size_t> global_scope;    // element -> ba type id
+	subtree_map<node, size_t> types;           // var -> ba type id
+	subtree_map<node, size_t> inputs;          // var -> stream name id
+	subtree_map<node, size_t> outputs;         // var -> stream name id
+	input_streams_remap       input_remaps;    // var name -> stream
+	output_streams_remap      output_remaps;   // var name -> stream
+	size_t type_of(tref var) const;
+	void update_types(const subtree_map<node, size_t>& global_scope);
+	tref add_input_console(const std::string& name, size_t type_id);
+	tref add_input_file(const std::string& name, size_t type_id, const std::string& filename);
+	tref add_output_console(const std::string& name, size_t type_id);
+	tref add_output_file(const std::string& name, size_t type_id, const std::string& filename);
+	tref add_input(const std::string& name, size_t type_id, std::shared_ptr<serialized_constant_input_stream> stream);
+	tref add_output(const std::string& name, size_t type_id, std::shared_ptr<serialized_constant_output_stream> stream);
+};
+
+template <NodeType node>
+std::ostream& operator<<(std::ostream& os, const io_context<node>& ctx);
+
+} // namespace idni::tau_lang
+
+#include "io_context.tmpl.h"
+
+#endif // __IDNI__TAU__IO_CONTEXT_H__
---
+++ src/io_context.tmpl.h
@@ -0,0 +1,215 @@
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.txt
+
+#include "io_context.h"
+#include "utility/term.h"
+
+#undef LOG_CHANNEL_NAME
+#define LOG_CHANNEL_NAME "io_context"
+
+namespace idni::tau_lang {
+
+// -----------------------------------------------------------------------------
+// constant streams
+// -----------------------------------------------------------------------------
+
+std::shared_ptr<serialized_constant_input_stream>
+	console_input_stream::rebuild()
+{
+	return std::make_shared<console_input_stream>();
+}
+
+std::optional<std::string> console_input_stream::get() {
+	std::string line;
+	term::enable_getline_mode();
+	std::getline(std::cin, line);
+	term::disable_getline_mode();
+	return line;
+}
+
+std::shared_ptr<serialized_constant_output_stream>
+	console_output_stream::rebuild()
+{
+	return std::make_shared<console_output_stream>();
+}
+
+bool console_output_stream::put(const std::string& value) {
+	std::cout << value << std::endl;
+	return true;
+}
+
+file_input_stream::file_input_stream(const std::string& filename)
+	: serialized_constant_input_stream(), filename(filename)
+{
+	file.open(filename);
+	if (!file.is_open())
+		LOG_ERROR << "Failed to reopen file: '" << filename << "'";
+}
+
+file_input_stream::~file_input_stream() {
+	if (file.is_open()) file.close();
+}
+
+std::shared_ptr<serialized_constant_input_stream>
+	file_input_stream::rebuild()
+{
+	return std::make_shared<file_input_stream>(filename);
+}
+
+std::optional<std::string> file_input_stream::get() {
+	std::string line;
+	std::getline(file, line);
+	DBG(LOG_TRACE << "file_input_stream(\"" << filename << "\"): get() = \"" << line << "\"";)
+	return line;
+}
+
+file_output_stream::file_output_stream(const std::string& filename)
+	: serialized_constant_output_stream(), filename(filename)
+{
+	DBG(LOG_TRACE << "file_output_stream(\"" << filename << "\"): open";)
+	file.open(filename);
+	if (!file.is_open())
+		LOG_ERROR << "Failed to open file: '" << filename << "'";
+}
+
+std::shared_ptr<serialized_constant_output_stream>
+	file_output_stream::rebuild()
+{
+	DBG(LOG_TRACE << "file_output_stream(\"" << filename << "\"): rebuild";)
+	return std::make_shared<file_output_stream>(filename);
+}
+
+file_output_stream::~file_output_stream() {
+	DBG(LOG_TRACE << "file_output_stream(\"" << filename << "\"): close";)
+	if (file.is_open()) file.close();
+}
+
+bool file_output_stream::put(const std::string& value) {
+	DBG(LOG_TRACE << "file_output_stream(\"" << filename << "\"): put(\"" << value << "\")";)
+	file << value << std::endl;
+	return true;
+}
+
+vector_input_stream::vector_input_stream(const std::vector<std::string>& values)
+	: serialized_constant_input_stream(), values(values)
+{
+	DBG(LOG_TRACE << "vector_input_stream::vector_input_stream() values.size(): " << values.size();)
+	for (const auto& value : values) {
+		DBG(LOG_TRACE << "vector_input_stream::vector_input_stream() value: " << value << "\n";)
+	}
+}
+
+std::shared_ptr<serialized_constant_input_stream>
+	vector_input_stream::rebuild()
+{
+	DBG(LOG_TRACE << "vector_input_stream::rebuild() values.size(): " << values.size();)
+	return std::make_shared<vector_input_stream>(values);
+}
+
+std::optional<std::string> vector_input_stream::get() {
+	if (current >= values.size()) return {};
+	DBG(LOG_TRACE << "vector_input_stream::get() = \"" << values[current] << "\" current: " << current << " values.size(): " << values.size();)
+	return values[current++];
+}
+
+vector_output_stream::vector_output_stream()
+	: serialized_constant_output_stream()
+{
+	DBG(LOG_TRACE << "vector_output_stream::vector_output_stream()";)
+}
+
+std::shared_ptr<serialized_constant_output_stream>
+	vector_output_stream::rebuild()
+{
+	DBG(LOG_TRACE << "vector_output_stream::rebuild()";)
+	return std::make_shared<vector_output_stream>();
+}
+
+bool vector_output_stream::put(const std::string& value) {
+	values.push_back(value);
+	DBG(LOG_TRACE << "vector_output_stream::put(\"" << values.back() << "\") values.size(): " << values.size();)
+	return true;
+}
+
+std::vector<std::string> vector_output_stream::get_values() const {
+	DBG(LOG_TRACE << "vector_output_stream::get_values() values.size(): " << values.size();)
+	for (const auto& value : values) {
+		DBG(LOG_TRACE << "vector_output_stream::get_values() value: " << value << "\n";)
+	}
+	return values;
+}
+
+// -----------------------------------------------------------------------------
+// IO context
+// -----------------------------------------------------------------------------
+
+template <NodeType node>
+size_t io_context<node>::type_of(tref var) const {
+	if (auto it = types.find(canonize<node>(var));
+		it != types.end()) return it->second;
+	return 0;
+}
+
+template <NodeType node>
+void io_context<node>::update_types(
+	const subtree_map<node, size_t>& update)
+{
+	// update types of inputs and outputs
+	// and create a default console stream if not defined
+	for (auto& [var, type] : update) if (is_io_var<node>(var)) {
+		types[var] = type;
+		std::string name = get_var_name<node>(var);
+		DBG(LOG_TRACE << "updating stream: " << name;)
+		bool is_input = name == "this" || name[0] == 'i';
+		DBG(assert(is_input || name == "u" || name[0] == 'o');) // TODO: should this raise an undefined io stream error?
+		auto& streams = is_input ? inputs : outputs;
+		if (streams.find(var) == streams.end()) streams[var] = 0;
+	}
+}
+
+template <NodeType node>
+tref io_context<node>::add_input_console(const std::string& name, size_t type_id) {
+	tref var = build_canonized_io_var<node>(name);
+	types.emplace(var, type_id);
+	inputs.emplace(var, 0);
+	return var;
+}
+
+template <NodeType node>
+tref io_context<node>::add_input_file(const std::string& name, size_t type_id, const std::string& filename) {
+	tref var = build_canonized_io_var<node>(name);
+	types.emplace(var, type_id);
+	inputs.emplace(var, dict(filename));
+	return var;
+}
+
+template <NodeType node>
+tref io_context<node>::add_output_console(const std::string& name, size_t type_id) {
+	tref var = build_canonized_io_var<node>(name);
+	types.emplace(var, type_id);
+	outputs.emplace(var, 0);
+	return var;
+}
+
+template <NodeType node>
+tref io_context<node>::add_output_file(const std::string& name, size_t type_id, const std::string& filename) {
+	tref var = build_canonized_io_var<node>(name);
+	types.emplace(var, type_id);
+	outputs.emplace(var, dict(filename));
+	return var;
+}
+
+template <NodeType node>
+tref io_context<node>::add_input(const std::string& name, size_t type_id, std::shared_ptr<serialized_constant_input_stream> stream) {
+	tref var = add_input_console(name, type_id);
+	input_remaps.emplace(name, stream);
+	return var;
+}
+
+template <NodeType node>
+tref io_context<node>::add_output(const std::string& name, size_t type_id, std::shared_ptr<serialized_constant_output_stream> stream) {
+	tref var = add_output_console(name, type_id);
+	output_remaps.emplace(name, stream);
+	return var;
+}
+
+} // namespace idni::tau_lang
