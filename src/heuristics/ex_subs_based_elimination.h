46d9a24cf2fabc7ec8acdbfdc9362b97a8cd6bdb
castrod <david.castroesteban@gmail.com>
2026-01-19 17:43:42 +0100

    adding es_subs_based_elimination sketch

---
+++ src/heuristics/ex_subs_based_elimination.h
@@ -0,0 +1,14 @@
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.md
+
+#ifndef __IDNI__TAU__EX_SUBS_BASED_ELIMINATION_H__
+#define __IDNI__TAU__EX_SUBS_BASED_ELIMINATION_H__
+
+#include "tau_tree.h"
+
+namespace idni::tau_lang {
+
+tref ex_subs_based_elimination(tref ex_clause, tref var);
+
+} // namespace idni::tau_lang
+
+#endif // __IDNI__TAU__EX_SUBS_BASED_ELIMINATION_H__
---
+++ tests/unit/test_heuristics-ex_subs_based_elimination.cpp
@@ -0,0 +1,8 @@
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.md
+
+#include "test_init.h"
+#include "heuristics/ex_subs_based_elimination.h"
+
+TEST_SUITE("ex_subs_based_elimination") {
+	TEST_CASE("ex_subs_based_elimination") { }
+}
