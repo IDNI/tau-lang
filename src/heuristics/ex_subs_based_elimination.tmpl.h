dcaf6da7548e0859a550881f9dc39fdd0186bcee
castrod <david.castroesteban@gmail.com>
2026-01-19 17:43:42 +0100

    adding es_subs_based_elimination sketch

---
+++ src/heuristics/ex_subs_based_elimination.tmpl.h
@@ -0,0 +1,9 @@
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.md
+
+#include "ex_subs_based_elimination.h"
+
+tref ex_subs_based_elimination(tref ex_clause, tref var)
+{
+	// TODO (HIGH) implement this function
+	return ex_clause;
+}
