c1d3dadd079b590fe754c1ac0c63923467ccd04d
tklip <tomas@idni.org>
2026-01-21 14:01:07 +0100

    reduce cmake global variables pollution

--- cmake/generate-parser.cmake
+++ cmake/generate-parser.cmake
@@ -1,9 +1,6 @@
 cmake_minimum_required(VERSION 3.22.1 FATAL_ERROR)
 
-set(GEN_DIR "${PROJECT_SOURCE_DIR}/parser")
-set(GEN_EXECUTABLE "${GEN_DIR}/gen")
-
 function(generate_parser tgf_filename)
-	execute_process(COMMAND ${GEN_EXECUTABLE} "${tgf_filename}"
+	execute_process(COMMAND "${PROJECT_SOURCE_DIR}/parser/gen" "${tgf_filename}"
 		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
 endfunction(generate_parser)
