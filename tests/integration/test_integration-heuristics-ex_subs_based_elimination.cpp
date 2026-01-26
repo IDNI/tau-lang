a05e42cc0ef25dd1ab8b7a4989f50355c0c6f90f
castrod <david.castroesteban@gmail.com>
2026-01-26 14:45:33 +0100

    adding ex quants by subs (preliminary)

--- tests/integration/test_integration-heuristics-ex_subs_based_elimination.cpp
+++ tests/integration/test_integration-heuristics-ex_subs_based_elimination.cpp
@@ -14,6 +14,13 @@ tref parse(const std::string& sample) {
 	return src;
 }
 
+TEST_SUITE("configuration") {
+
+	TEST_CASE("logging") {
+		logging::trace();
+	}
+}
+
 TEST_SUITE("ex_subs_based_elimination") {
 
 	TEST_CASE("simple case subs applied (y1)") {
@@ -46,6 +53,15 @@ TEST_SUITE("ex_subs_based_elimination") {
 	TEST_CASE("simple case no subs applied (y2)") {
 		const char* sample =
 			"(x = a || x = c) && x & b = 1";
+		auto var = build_variable<node_t>("x", tau_type_id<node_t>()); // tau typed variable 'x'
+		tref ex_clause = parse(sample);
+		tref result = ex_subs_based_elimination<node_t>(ex_clause, var);
+		CHECK( result == ex_clause );
+	}
+
+	TEST_CASE("simple case neg no subs applied (y2)") {
+		const char* sample =
+			"x' & b = 1";
 		auto var = build_variable<node_t>("x", tau_type_id<node_t>()); // tau typed variable 'x'
 		tref ex_clause = parse(sample);
 		tref result = ex_subs_based_elimination<node_t>(ex_clause, var);
