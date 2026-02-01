23281ffc507c5e3b80e713a9b0e15e090c3153ad
tklip <tomas@idni.org>
2026-02-01 20:03:16 +0100

    repl tests: cleaning, one test file per command

--- tests/repl/commands/test_repl-normalize_cmd.cmake
+++ tests/repl/commands/test_repl-normalize_cmd.cmake
@@ -4,15 +4,26 @@
 
 include(add_repl_test)
 
-#test_repl-nnf_bf_cmd
-#add_repl_test(normalize_cmd "normalize X  & (Y | Z). q" "X & Y | X & Z")
+# term
+add_repl_test(normalize_cmd_bf "n X & 1" ": X")
+add_repl_test(normalize_cmd_nso "n X & X' = 0" ": T")
 
-# 'normalize' command for Boolean functions
+# formula
+add_repl_test(normalize_cmd_wff "n X & X' = 0" ": T")
+add_repl_test(normalize_cmd_memory "X & X' = 0. n %" ": T")
+
+# function applications
+add_repl_test(normalize_cmd_func_app_1 "f(x) := x'. n f(t)" "t'")
+
+# predicate applications
+add_repl_test(normalize_cmd_pred_app_1 "p(x) := x' = 0. n p(t)" "t' = 0")
+
+# 'normalize' command for terms with history reference
 add_repl_test(normalize_cmd_bf_nonmem "normalize 1" ": 1")
 add_repl_test(normalize_cmd_bf_mem_rel "1. normalize %-0" "1")
 add_repl_test(normalize_cmd_bf_mem_abs "1. normalize %1" "1")
 
-# 'n' command for normalization for Boolean functions
+# 'normalize' command for normalization for formulas with history reference
 add_repl_test(normalize_cmd_wff_nonmem "normalize T" ": T")
 add_repl_test(normalize_cmd_wff_mem_rel "T. normalize %-0" "T")
 add_repl_test(normalize_cmd_wff_mem_abs "T. normalize %1" "T")
