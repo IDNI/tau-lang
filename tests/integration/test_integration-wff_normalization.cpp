97293729766f61a025daa561e3ab5125c009815c
tklip <tomas@idni.org>
2026-02-05 16:24:20 +0100

    `definitions` node is optional, don't include it in the parse tree if empty

--- tests/integration/test_integration-wff_normalization.cpp
+++ tests/integration/test_integration-wff_normalization.cpp
@@ -61,6 +61,7 @@ TEST_SUITE("syntactic_path_simplification") {
 		tref fm = get_bf_nso_rr("", sample).value().main->get();
 		tref res = syntactic_path_simplification<node_t>::on(fm);
 		CHECK( matches_to_str_to_any_of(res, {
+			"x&(z'|yk)|yx|zk",
 			"x&(z'|ky)|xy|zk",
 			"x&(z'|ky)|yx|zk",
 		}) );
@@ -85,8 +86,8 @@ TEST_SUITE("simplify_using_equality") {
 		tref fm = get_nso_rr(sample).value().main->get();
 		tref res = simplify_using_equality<node_t>::on(fm);
 		CHECK( matches_to_str_to_any_of(res, {
-			"xy|zx = 0",
 			"yx|zx = 0",
+			"xy|zx = 0",
 			"xy|xz = 0",
 			"yx|xz = 0",
 		}) );
@@ -120,6 +121,7 @@ TEST_SUITE("simplify_using_equality") {
 		tref fm = get_nso_rr(sample).value().main->get();
 		tref res = simplify_using_equality<node_t>::on(fm);
 		CHECK( matches_to_str_to_any_of(res, {
+			"yx = 0 && wv = 0 && yw = 0 && vx = 0",
 			"xy = 0 && vw = 0 && wy = 0 && vx = 0",
 			"yx = 0 && wv = 0 && wy = 0 && vx = 0",
 			"xy = 0 && wv = 0 && wy = 0 && xv = 0",
@@ -270,6 +272,7 @@ TEST_SUITE("boole_normal_form") {
 		tref fm = get_nso_rr(sample).value().main->get();
 		tref res = boole_normal_form<node_t>(fm);
 		CHECK( matches_to_str_to_any_of(res, {
+			"x'b'a|xba' = 0 || b&(x'|a)|xb'a != 0",
 			"a'xb|ax'b' = 0 || a&(x|b)|a'x'b != 0",
 			"b'ax'|ba'x = 0 || b&(a|x')|b'ax != 0",
 			"b'x'a|bxa' = 0 || b&(x'|a)|b'xa != 0",
