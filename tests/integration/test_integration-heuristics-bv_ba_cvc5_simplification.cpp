4c7a2d583e646184c909650d6b430c463f432f88
castrod <david.castroesteban@gmail.com>
2026-02-09 13:50:36 +0100

    adding ba bv cvc5 constant simplification test suite

--- tests/integration/test_integration-heuristics-bv_ba_cvc5_simplification.cpp
+++ tests/integration/test_integration-heuristics-bv_ba_cvc5_simplification.cpp
@@ -25,14 +25,159 @@ tref parse_bf(const std::string& sample) {
 	return src;
 }
 
-TEST_SUITE("ba bv cvc5 simplification") {
+TEST_SUITE("ba bv cvc5 constant simplification") {
 
-	TEST_CASE("1 + 2") {
-		const char* sample = "{1}:bv[64] + {2}:bv[64]";
+	TEST_CASE("addition of constants") {
+		logging::trace();
+		const char* sample = "{1}:bv[8] + {2}:bv[8]";
 		tref src = parse_bf(sample);
 		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
 		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
-		CHECK( true );
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
 	}
 
+	TEST_CASE("substraction of constants") {
+		logging::trace();
+		const char* sample = "{1}:bv[8] - {2}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("multiplication of constants") {
+		logging::trace();
+		const char* sample = "{3}:bv[8] * {4}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("division of constants") {
+		logging::trace();
+		const char* sample = "{10}:bv[8] / {2}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("mod of constants") {
+		logging::trace();
+		const char* sample = "{10}:bv[8] % {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("shift right of constants") {
+		logging::trace();
+		const char* sample = "{16}:bv[8] >> {2}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("shift left of constants") {
+		logging::trace();
+		const char* sample = "{1}:bv[8] << {2}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("not of constant") {
+		logging::trace();
+		const char* sample = "{0}:bv[8]'";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified == src) ;
+		logging::info();
+	}
+
+	TEST_CASE("and of constants") {
+		logging::trace();
+		const char* sample = "{5}:bv[8] & {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("nand of constants") {
+		logging::trace();
+		const char* sample = "{5}:bv[8] !& {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("or of constants") {
+		logging::trace();
+		const char* sample = "{5}:bv[8] | {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("nor of constants") {
+		logging::trace();
+		const char* sample = "{5}:bv[8] !| {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("xor of constants") {
+		logging::trace();
+		const char* sample = "{5}:bv[8] ^ {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
+
+	TEST_CASE("xnor of constants") {
+		logging::trace();
+		const char* sample = "{5}:bv[8] !^ {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref simplified = bv_ba_cvc5_simplification<node_t>(src);
+		TAU_LOG_TRACE << "simplified: " << tree<node_t>::get(simplified).tree_to_str() << "\n";
+		CHECK( simplified != nullptr );
+		CHECK( simplified != src) ;
+		logging::info();
+	}
 }
