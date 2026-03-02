1908e8fb62fab08c2988478a5d2c5e7844862c1c
castrod <david.castroesteban@gmail.com>
2026-03-02 11:27:23 +0100

    basic bf_blastin implementation

---
+++ tests/integration/test_integration-heuristics-bv_bitblasting.cpp
@@ -0,0 +1,38 @@
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.md
+
+#include "test_init.h"
+#include "test_tau_helpers.h"
+
+#include "boolean_algebras/bv_ba.h"
+
+TEST_SUITE("configuration") {
+
+	TEST_CASE("bdd_init") {
+		bdd_init<Bool>();
+	}
+
+	TEST_CASE("logging") {
+		logging::trace();
+	}
+}
+
+tref parse_bf(const std::string& sample) {
+	static tree<node_t>::get_options opts{ .parse = { .start = tree<node_t>::bf }};
+	auto src = tree<node_t>::get(sample, opts);
+	if (src == nullptr) {
+		TAU_LOG_ERROR << "Parsing failed for: " << sample;
+	}
+	return src;
+}
+
+TEST_SUITE("bv blasting") {
+
+	TEST_CASE("x | y = 0") { }
+}
+
+TEST_SUITE("Cleanup") {
+
+	TEST_CASE("ba_constants cleanup") {
+		ba_constants<node_t>::cleanup();
+	}
+}
