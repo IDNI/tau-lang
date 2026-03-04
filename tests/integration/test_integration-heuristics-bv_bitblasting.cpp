c78564525bdcdc498a82db72e9336b2dbe375cb3
castrod <david.castroesteban@gmail.com>
2026-03-04 12:46:16 +0100

    fixing more compilation issues

--- tests/integration/test_integration-heuristics-bv_bitblasting.cpp
+++ tests/integration/test_integration-heuristics-bv_bitblasting.cpp
@@ -17,8 +17,8 @@ TEST_SUITE("configuration") {
 	}
 }
 
-tref parse_bf(const std::string& sample) {
-	static tree<node_t>::get_options opts{ .parse = { .start = tree<node_t>::bf }};
+tref parse_wff(const std::string& sample) {
+	static tree<node_t>::get_options opts{ .parse = { .start = tree<node_t>::wff }};
 	auto src = tree<node_t>::get(sample, opts);
 	if (src == nullptr) {
 		TAU_LOG_ERROR << "Parsing failed for: " << sample;
@@ -28,7 +28,20 @@ tref parse_bf(const std::string& sample) {
 
 TEST_SUITE("bv blasting") {
 
-	TEST_CASE("x | y = 0") { }
+	TEST_CASE("x | y = 0") {
+		logging::trace();
+		tref src = parse_wff("x:bv[64] | y = 0");
+		auto solution = bv_bitblasting_solve<node_t>(src);
+		if (solution) {
+			TAU_LOG_INFO << "Solution found for: " << src;
+			for (const auto& [var, val] : solution.value()) {
+				TAU_LOG_INFO << "  " << var << " -> " << val;
+			}
+		} else {
+			TAU_LOG_INFO << "No solution found for: " << src;
+		}
+		logging::info();
+	}
 }
 
 TEST_SUITE("Cleanup") {
