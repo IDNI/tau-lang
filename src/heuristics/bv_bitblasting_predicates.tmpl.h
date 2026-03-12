def5e48da2901125622431ef8a9f18c9b4a954d4
castrod <david.castroesteban@gmail.com>
2026-03-12 12:31:12 +0100

    further speccing out predicate blasting

--- src/heuristics/bv_bitblasting_predicates.tmpl.h
+++ src/heuristics/bv_bitblasting_predicates.tmpl.h
@@ -9,233 +9,15 @@ using bool_node = idni::tau_lang::node<cvc5::Term, Bool>;
 using bool_tree = tree<bool_node>;
 static auto bool_id = bool_type_id<bool_node>();
 
-
-/*using bool_node = idni::tau_lang::node<cvc5::Term, Bool>;
-using bool_tree = tree<bool_node>;
-using bitblast_solution = solution<bool_node>;
-static auto bool_id = bool_type_id<bool_node>();
-
 template<NodeType node>
-tref basic_bitblast(const typename tree<node>::traverser& term) {
-	using tau = tree<node>;
-	using tt = tau::traverser;
-
-	auto t = tau::get(tt(term) | tt::ref);
-	bool_node new_node{t.value.nt, t.value.data, t.value.term, t.value.ba_type, t.value.ext};
-	return bool_tree::get(new_node, t.get_children());
-}
-
-// bf_bitblast returns a vector of bits representing the bitblasted term, where
-// the i-th element of the vector corresponds to the i-th bit of the original
-// term (0-indexed from the most significant bit).
-template<NodeType node>
-std::optional<trefs> bf_bitblast(const typename tree<node>::traverser& term) {
-	using tau = tree<node>;
-	using tt = tau::traverser;
-
-	static auto one = bool_tree::get_typed(bool_tree::bf_t, bool_id);
-	static auto zero = bool_tree::get_typed(bool_tree::bf_f, bool_id);
-
-	auto nt = term | tt::nt;
-	auto ref = term | tt::ref;
-	auto n = tau::get(ref);
-	auto type = n.get_ba_type();
-	auto bv_width = get_bv_width<node>(type);
-
-	switch (nt) {
-		case tau::bf_add: case tau::bf_sub: case tau::bf_mul:
-		case tau::bf_div: case tau::bf_mod: case tau::bf_shl:
-		case tau::bf_shr: case tau::bf_nand: case tau::bf_xnor:
-		case tau::bf_nor:{
-			// Unsupported operations for now
-			LOG_ERROR << "Arithmetic operations are not supported in bitblasting yet.";
-			return std::nullopt;
-		}
-		case tau::bf: {
-			auto bits = bf_bitblast<node>(term | tt::first);
-			if (!bits) return std::nullopt;
-			trefs result;
-			for (size_t i = 0; i < bv_width; ++i)
-				result.push_back(bool_tree::get(bool_tree::bf, bits.value()[i]));
-			return result;
-		}
-		case tau::bf_neg: {
-			auto bits = bf_bitblast<node>(term | tt::first);
-			if (!bits) return std::nullopt;
-			trefs result;
-			for (size_t i = 0; i < bv_width; ++i) {
-				result.push_back(bool_tree::get(bool_tree::bf_neg, bits.value()[i]));
-			}
-			return result;
-		}
-		case tau::bf_and: case tau::bf_or: case tau::bf_xor: {
-			auto left_bits = bf_bitblast<node>(term | tt::first);
-			auto right_bits = bf_bitblast<node>(term | tt::second);
-			if (!left_bits || !right_bits) return std::nullopt;
-			trefs result;
-			for (size_t i = 0; i < bv_width; ++i)
-				result.push_back(bool_tree::get(nt, left_bits.value()[i], right_bits.value()[i]));
-			return result;
-		}
-		case tau::bf_t:	case tau::bf_f: {
-			trefs result;
-			for (size_t i = 0; i < bv_width; ++i)
-				result.push_back(nt == bool_tree::bf_t ? one : zero);
-			return result;
-			}
-		case tau::ba_constant: {
-			auto cte = term | tt::ba_constant;
-			DBG(assert(std::holds_alternative<bv>(cte));)
-			auto str = std::get<bv>(cte).getBitVectorValue(2);
-			trefs result;
-			for (size_t i = 0; i < bv_width; ++i)
-				result.push_back(str[i] == '1' ? one : zero);
-			return result;
-		}
-		case tau::variable: case tau::ctnvar: {
-			auto booled = basic_bitblast<node>(term);
-			auto booled_node = bool_tree::get(booled);
-			auto bit = bool_tree::get(booled_node.value.ba_retype(bool_id), booled_node.get_children());
-			trefs result;
-			for (size_t i = 0; i < bv_width; ++i) {
-				result.push_back(bool_tree::add_child(bit,
-					bool_tree::get(bool_tree::offset, bool_tree::get_num(i))));
-			}
-			return result;
-		}
-		default:
-			// Unsupported operations for now
-			LOG_ERROR << "Unsupported operation in bitblasting: " << LOG_FM(tt(term)|| tt::ref);
-			return std::nullopt;
-	}
-}
-
-template<NodeType node>
-tref wff_bitblast(const typename tree<node>::traverser& term) {
-	using tau = tree<node>;
-	using tt = tau::traverser;
-
-	static auto one = bool_tree::get_typed(bool_tree::bf_t, bool_id);
-
-	auto nt = term | tt::nt;
-	auto ref = term | tt::ref;
-	auto n = tau::get(ref);
-
-	switch (nt) {
-		case tau::wff:
-		return bool_tree::get(bool_tree::wff, wff_bitblast<node>(term | tt::first));
-		case tau::wff_sometimes: case tau::wff_always:
-		case tau::wff_all: case tau::wff_ex:
-		case tau::wff_neg: case tau::wff_or: case tau::wff_conditional:
-		case tau::wff_imply: case tau::wff_rimply: case tau::wff_equiv:
-		case tau::wff_xor: case tau::wff_and: case tau::bf_interval:
-		case tau::bf_lt: case tau::bf_gt: case tau::bf_lteq: case tau::bf_gteq:
-		case tau::bf_nlt: case tau::bf_ngt: case tau::bf_nlteq: case tau::bf_ngteq: {
-			// Unsupported operations for now
-			LOG_ERROR << "Unsupported operation in wff bitblasting: " << LOG_NT(nt) << " " << LOG_FM(tt(term)|| tt::ref);
-			return nullptr;
-		}
-		case tau::bf_eq: case tau::bf_neq: {
-			auto type = n.get_ba_type();
-			auto bv_width = get_bv_width<node>(type);
-			auto left = bf_bitblast<node>(term | tt::first);
-			auto right = bf_bitblast<node>(term | tt::second);
-			if (!left || !right) return nullptr;
-			tref result = bool_tree::_0(bool_id);
-			for (size_t i = 0; i < bv_width; ++i) {
-				result = build_bf_or<bool_node>(
-					result,
-					build_bf_xor<bool_node>(left.value()[i], right.value()[i])
-				);
-			}
-			return (nt == tau::bf_eq)
-				? build_bf_eq_0<bool_node>(result)
-				: build_bf_eq_0<bool_node>(build_bf_xor<bool_node>(result, one));
-		}
-		case tau::wff_t: case tau::wff_f: {
-			return ref;
-		}
-		default: {
-			// Unsupported operations for now
-			LOG_ERROR << "Unsupported operation in wff bitblasting: " << LOG_NT(nt) << " " << LOG_FM(tt(term)|| tt::ref);
-			return nullptr;
-		}
-	}
-}
-
-template<NodeType node>
-std::optional<tref> bitblast(tref term) {
-	using tau = tree<node>;
-	using tt = tau::traverser;
-
-	return wff_bitblast<node>(tt(term));
-}
-
-template<NodeType node>
-std::optional<solution<node>> unbitblast([[maybe_unused]] bitblast_solution& solution) {
-	return std::nullopt;
-}
-
-template<NodeType node>
-std::optional<solution<node>> bv_bitblasting_solve(tref term) {
-	auto bitblasted = bitblast<node>(term);
-	if (!bitblasted) {
-		LOG_ERROR << "Bitblasting failed for term: " << LOG_FM(term);
-		return std::nullopt;
-	}
-
-	DBG(LOG_TRACE << "bv_bitblasting_solve/bitblasted: " << LOG_FM(bitblasted.value());)
-
-	auto bool_solution = lgrs<bool_node>(bitblasted.value());
-	if (!bool_solution) {
-		LOG_ERROR << "Solving bitblasted term failed for: " << LOG_FM(term);
-		return std::nullopt;
-	}
-
-	DBG(LOG_TRACE << "bv_bitblasting_solve/bool_solution: " << dump_to_str(bool_solution.value());)
-
-	auto solution = unbitblast<node>(bool_solution.value());
-	if (!solution) {
-		LOG_ERROR << "Unbitblasting failed for term: " << LOG_FM(term);
-		return std::nullopt;
-	}
-
-	DBG(LOG_TRACE << "bv_bitblasting_solve/solution: " << dump_to_str(solution.value());)
-
-	return solution;
-}
-
-template<NodeType node>
-bool bv_bitblasting_sat(tref term) {
-	return bitblast<node>(term);
-}
-
-template<NodeType node>
-bool bv_bitblasting_unsat(tref term) {
-	return bitblast<node>(term);
-}
-
-template<NodeType node>
-bool bv_bitblasting_valid(tref term) {
-	return bitblast<node>(term);
-}*/
-
-template<NodeType node>
-tref bvadd_predicate([[maybe_unused]] tref result, [[maybe_unused]] tref var) {
+tref bvadd_predicate([[maybe_unused]] tref left, [[maybe_unused]] tref right, [[maybe_unused]] tref var) {
 	// Unsupported operations for now
 	LOG_ERROR << "Not yet implemented.";
 	return nullptr;
 }
 
 template<NodeType node>
-tref bvadd_predicate([[maybe_unused]] tref left, [[maybe_unused]] tref right, [[maybe_unused]]	 tref var) {
-	// Unsupported operations for now
-	LOG_ERROR << "Not yet implemented.";
-	return nullptr;
-}
-
-template<NodeType node>
-tref bvsub_predicate([[maybe_unused]] tref result, [[maybe_unused]] tref var) {
+tref bvmul_predicate([[maybe_unused]] tref left, [[maybe_unused]] tref right, [[maybe_unused]] tref var) {
 	// Unsupported operations for now
 	LOG_ERROR << "Not yet implemented.";
 	return nullptr;
@@ -243,6 +25,13 @@ tref bvsub_predicate([[maybe_unused]] tref result, [[maybe_unused]] tref var) {
 
 template<NodeType node>
 tref bvsub_predicate([[maybe_unused]] tref left, [[maybe_unused]] tref right, [[maybe_unused]] tref var) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref bvdiv_predicate([[maybe_unused]] tref left, [[maybe_unused]] tref right, [[maybe_unused]] tref var) {
 	// Unsupported operations for now
 	LOG_ERROR << "Not yet implemented.";
 	return nullptr;
@@ -263,114 +52,211 @@ tref bvshl_one_predicate([[maybe_unused]] tref left, [[maybe_unused]] tref var) 
 }
 
 template<NodeType node>
-tref atomic_predicate_blasting(tref term) {
-	using tau = tree<node>;
-	//using tt = tau::traverser;
+tref eq_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
 
-	subtree_map<node, tref> changes;
-	tref predicate = nullptr;
+template<NodeType node>
+tref neq_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref lt_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref gt_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref lteq_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref gteq_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref nlt_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref ngt_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref nlteq_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref ngteq_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref interval_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
+	// Unsupported operations for now
+	LOG_ERROR << "Not yet implemented.";
+	return nullptr;
+}
+
+template<NodeType node>
+tref bf_predicate_blasting(tref term, subtree_map<node, tref>& changes, trefs& vars) {
+	using tau = tree<node>;
+
+	tref predicate;
+	bool error = false;
+	auto type_id = tau::get(term).get_ba_type();
 
 	auto f = [&](tref t) {
 		auto nt = tau::get(t).get_type();
-		auto type_id = tau::get(t).get_ba_type();
 
 		switch (nt) {
 			case tau::bf_add: case tau::bf_sub: {
 				auto var = build_variable<node>(type_id);
-				auto left = tau::get(t).child(0);
-				auto right = tau::get(t).child(1);
+				vars.push_back(var);
 				changes[t] = var;
-				auto current_predicate = (nt == tau::bf_add)
+				auto left = changes[tau::get(t).child(0)];
+				auto right = changes[tau::get(t).child(1)];
+				auto current = (nt == tau::bf_add)
 					? bvadd_predicate<node>(left, right, var)
 					: bvsub_predicate<node>(left, right, var);
-				auto subformula = predicate
-					? build_bf_and<node>(predicate, current_predicate)
-					: current_predicate;
-				predicate = build_wff_ex<node>(var, subformula);
+				predicate = predicate
+					? build_bf_and<node>(predicate, current)
+					: current;
 				break;
 			}
 			case tau::bf_shr: case tau::bf_shl: {
+				// TODO (HIGH) check for 1 in the right operand
 				auto var = build_bf_var<node>(type_id);
+				vars.push_back(var);
 				auto left = tau::get(t).child(0);
 				changes[t] = var;
-				auto current_predicate = (nt == tau::bf_shl)
+				auto current = (nt == tau::bf_shl)
 					? bvshl_one_predicate<node>(left, var)
 					: bvrhl_one_predicate<node>(left, var);
-				auto subformula = predicate
-					? build_bf_and<node>(predicate, current_predicate)
-					: current_predicate;
-				predicate = build_wff_ex<node>(var, subformula);
+				predicate = predicate
+					? build_bf_and<node>(predicate, current)
+					: current;
 				break;
 			}
 			case tau::bf_mul: case tau::bf_div: case tau::bf_mod:
 			case tau::bf_nand: case tau::bf_xnor: case tau::bf_nor:{
 				// Unsupported operations for now
-				LOG_ERROR << "Arithmetic operations are not supported yet predicate blasting.";
-				return false;
+				LOG_ERROR << "Operation "
+					<< LOG_NT(nt) << " not supported yet in predicate blasting.";
+				return error = true, false;
 			}
 			default: {
-				auto n_t = rewriter::replace<node>(t, changes);
-				if (n_t != t) changes[t] = n_t;
 				break;
 			}
 		}
 		return true;
 	};
 
-	post_order<node>(term).search_unique(f);
+	// if we have an unsupported operation, i.e. we have an unsupported operation
+	//we return nullptr to indicate failure
+	if (error) return nullptr;
+	return predicate;
+}
+template<NodeType node>
+tref atomic_predicate_blasting(tref atomic) {
 
-	if (changes.find(term) != changes.end()) {
-		DBG(LOG_TRACE << "bv_predicate_blasting/term -> changes[term]: "
-			<< LOG_FM(term) << " -> " << LOG_FM(changes[term]);)
-		return changes[term];
+	auto type_id = tau::get(atomic).get_ba_type();
+	if(!is_bv_type_family<node>(type_id))	return atomic;
+
+	auto atomic_nt = tau::get(atomic)[0].get_type();
+	tref predicate;
+	trefs vars;
+
+	tref current;
+	switch (atomic_nt) {
+		case tau::bf_eq: { current = eq_predicate<node>(atomic, vars); break; }
+		case tau::bf_neq: { current = neq_predicate<node>(atomic, vars); break; }
+		case tau::bf_lt: { current = lt_predicate<node>(atomic, vars); break; }
+		case tau::bf_gt: { current = gt_predicate<node>(atomic, vars); break; }
+		case tau::bf_lteq: { current = lteq_predicate<node>(atomic, vars); break; }
+		case tau::bf_gteq: { current = gteq_predicate<node>(atomic, vars); break; }
+		case tau::bf_nlt: { current = nlt_predicate<node>(atomic, vars); break; }
+		case tau::bf_ngt: { current = ngt_predicate<node>(atomic, vars); break; }
+		case tau::bf_nlteq: { current = nlteq_predicate<node>(atomic, vars); break; }
+		case tau::bf_ngteq: { current = ngteq_predicate<node>(atomic, vars); break; }
+		case tau::bf_interval: { current = interval_predicate<node>(atomic, vars); break; }
+		default: {
+			// error, unknown atomic predicate, we return nullptr to indicate failure
+			LOG_ERROR << "Unknown atomic predicate: " << LOG_NT(atomic_nt);
+			return nullptr;
+		}
 	}
-	return term;
+
+	predicate = predicate
+		? build_bf_and<node>(predicate, current)
+		: current;
+
+	// we add the existentials for the variables we introduced
+	for (auto it = vars.rbegin(); it != vars.rend(); ++it) {
+		predicate = build_wff_ex<node>(*it, predicate);
+	}
+
+	return predicate;
 }
 
 template<NodeType node>
 tref wff_predicate_blasting(tref term) {
 	using tau = tree<node>;
-	using tt = tau::traverser;
 
 	subtree_map<node, tref> changes;
-	auto one = bool_tree::_1(bool_id);
 
 	auto f = [&](tref t) {
 		auto nt = tau::get(t).get_type();
-		auto type_id = tau::get(t).get_ba_type();
 
 		switch (nt) {
 			case tau::bf_interval:
 			case tau::bf_lt: case tau::bf_gt: case tau::bf_lteq: case tau::bf_gteq:
-			case tau::bf_nlt: case tau::bf_ngt: case tau::bf_nlteq: case tau::bf_ngteq: {
-				// Unsupported operations for now
-				LOG_ERROR << "Unsupported comparison in wff predicate blasting: "
-					<< LOG_NT(nt) << " " << LOG_FM(tt(term)|| tt::ref);
-				return false;
-			}
+			case tau::bf_nlt: case tau::bf_ngt: case tau::bf_nlteq: case tau::bf_ngteq:
 			case tau::bf_eq: case tau::bf_neq: {
-				auto bv_width = get_bv_width<node>(type_id);
-				auto left = atomic_predicate_blasting<node>(tau::get(term)[0]);
-				auto right = atomic_predicate_blasting<node>(tau::get(term)[1]);
-				if (!left || !right) return nullptr;
-				tref result = bool_tree::_0(bool_id);
-				for (size_t i = 0; i < bv_width; ++i) {
-					result = build_bf_or<bool_node>(
-						result,
-						build_bf_xor<bool_node>(left.value()[i], right.value()[i])
-					);
-				}
-				return (nt == tau::bf_eq)
-					? build_bf_eq_0<bool_node>(result)
-					: build_bf_eq_0<bool_node>(build_bf_xor<bool_node>(result, one));
-			}
-			case tau::wff_t: case tau::wff_f: {
-				return ref;
+				changes[t] = atomic_predicate_blasting<node>(t);
+				break;
 			}
 			default: {
-				// Unsupported operations for now
-				LOG_ERROR << "Unsupported operation in wff bitblasting: " << LOG_NT(nt) << " " << LOG_FM(tt(term)|| tt::ref);
-				return false;
+				trefs ch;
+				for (tref c : tau::get(t).children()) {
+					if (changes.find(c) != changes.end())
+						ch.push_back(changes[c]);
+					else ch.push_back(c);
+				}
+
+				if (auto new_n = tau::get(tau::get(t).value, ch.data(), ch.size()); new_n != t)
+					changes.insert_or_assign(t, new_n);
+				break;
 			}
 		}
 		return true;
