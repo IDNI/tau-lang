37abf29d75ce7b44cf63249b777e7af5656219ec
castrod <david.castroesteban@gmail.com>
2026-03-16 10:36:26 +0100

    speccing out factory methods

--- src/heuristics/bv_bitblasting_predicates.tmpl.h
+++ src/heuristics/bv_bitblasting_predicates.tmpl.h
@@ -9,65 +9,51 @@ using bool_node = idni::tau_lang::node<cvc5::Term, Bool>;
 using bool_tree = tree<bool_node>;
 static auto bool_id = bool_type_id<bool_node>();
 
+// This is a factory for creating the rules needed for each bitvector operation.
+// We also include factory methods for creating the calls to easy the creation
+// of the predicates.
 template<NodeType node>
-tref bf_predicate_blasting(tref term, subtree_map<node, tref>& changes, trefs& vars) {
-	using tau = tree<node>;
+struct bv_bitblasting_rules {
 
-	tref predicate;
-	bool error = false;
-	auto type_id = tau::get(term).get_ba_type();
+	static rewriter::rules bvadd(size_t bitwidth);
+	static rewriter::rules bvsub(size_t bitwidth);
+	static rewriter::rules bvshl_by_one(size_t bitwidth);
+	static rewriter::rules bvrhl_by_one(size_t bitwidth);
+	static rewriter::rules bvmul(size_t bitwidth);
+	static rewriter::rules bvdiv(size_t bitwidth);
+	static rewriter::rules bvmod(size_t bitwidth);
+	static rewriter::rules bvshl(size_t bitwidth);
+	static rewriter::rules bvrhl(size_t bitwidth);
+	static rewriter::rules bit(size_t bitwidth, size_t bit);
 
-	auto f = [&](tref t) {
-		auto nt = tau::get(t).get_type();
+	static tref make_bvadd_call(size_t bitwidth);
+	static tref make_bvsub_call(size_t bitwidth);
+	static tref make_bvshl_by_one_call(size_t bitwidth);
+	static tref make_bvrhl_by_one_call(size_t bitwidth);
+	static tref make_bvmul_call(size_t bitwidth);
+	static tref make_bvdiv_call(size_t bitwidth);
+	static tref make_bvmod_call(size_t bitwidth);
+	static tref make_bvshl_call(size_t bitwidth);
+	static tref make_bvrhl_call(size_t bitwidth);
+	static tref make_bit_call(size_t bitwidth, size_t bit);
+};
 
-		switch (nt) {
-			case tau::bf_add: case tau::bf_sub: {
-				auto var = build_variable<node>(type_id);
-				vars.push_back(var);
-				changes[t] = var;
-				auto left = changes[tau::get(t).child(0)];
-				auto right = changes[tau::get(t).child(1)];
-				auto current = (nt == tau::bf_add)
-					? bvadd_predicate<node>(left, right, var)
-					: bvsub_predicate<node>(left, right, var);
-				predicate = predicate
-					? build_bf_and<node>(predicate, current)
-					: current;
-				break;
-			}
-			case tau::bf_shr: case tau::bf_shl: {
-				// TODO (HIGH) check for 1 in the right operand
-				auto var = build_bf_var<node>(type_id);
-				vars.push_back(var);
-				auto left = tau::get(t).child(0);
-				changes[t] = var;
-				auto current = (nt == tau::bf_shl)
-					? bvshl_one_predicate<node>(left, var)
-					: bvrhl_one_predicate<node>(left, var);
-				predicate = predicate
-					? build_bf_and<node>(predicate, current)
-					: current;
-				break;
-			}
-			case tau::bf_mul: case tau::bf_div: case tau::bf_mod:
-			case tau::bf_nand: case tau::bf_xnor: case tau::bf_nor:{
-				// Unsupported operation for now
-				LOG_ERROR << "Operation "
-					<< LOG_NT(nt) << " not supported yet in predicate blasting.";
-				return error = true, false;
-			}
-			default: {
-				break;
-			}
-		}
-		return true;
-	};
+// This is a factory for creating the predicates needed for each bitvector operation.
+// We also include factory methods for creating the calls to easy the creation of
+// calls. We aplly the above rules to create the final predicates
+// for the bitvector operations.
+struct bv_bitblasting_predicates {
 
-	// if we have an unsupported operation, i.e. we have an unsupported operation
-	//we return nullptr to indicate failure
-	if (error) return nullptr;
-	return predicate;
-}
+	static rewriter::rule bvadd_predicate(size_t bitwidth);
+	static rewriter::rule bvsub_predicate(size_t bitwidth);
+	static rewriter::rule bvshl_by_one_predicate(size_t bitwidth);
+	static rewriter::rule bvmul_predicate(size_t bitwidth);
+	static rewriter::rule bvdiv_predicate(size_t bitwidth);
+	static rewriter::rule bvmod_predicate(size_t bitwidth);
+	static rewriter::rule bvshl_predicate(size_t bitwidth);
+	static rewriter::rule bvrhl_predicate(size_t bitwidth);
+
+};
 
 
 template<NodeType node>
@@ -111,6 +97,74 @@ tref bvshl_one_predicate([[maybe_unused]] tref left, [[maybe_unused]] tref var) 
 	LOG_ERROR << "Not yet implemented.";
 	return nullptr;
 }
+
+template<NodeType node>
+tref bf_predicate_blasting(tref term, subtree_map<node, tref>& changes, trefs& vars) {
+	using tau = tree<node>;
+
+	tref predicate;
+	bool error = false;
+	auto type_id = tau::get(term).get_ba_type();
+
+	auto f = [&](tref t) {
+		auto nt = tau::get(t).get_type();
+
+		switch (nt) {
+			case tau::bf_add: case tau::bf_sub: {
+				auto var = build_variable<node>(type_id);
+				vars.push_back(var);
+				changes[t] = var;
+				auto left = changes[tau::get(t).child(0)];
+				auto right = changes[tau::get(t).child(1)];
+				auto current = (nt == tau::bf_add)
+					? bvadd_predicate<node>(left, right, var)
+					: bvsub_predicate<node>(left, right, var);
+				predicate = predicate
+					? build_bf_and<node>(predicate, current)
+					: current;
+				break;
+			}
+			case tau::bf_shl: case tau::bf_shr: {
+				// TODO (HIGH) check for 1 in the by one shift
+				// operations and for general case when we support it.
+				auto var = build_bf_var<node>(type_id);
+				vars.push_back(var);
+				auto left = tau::get(t).child(0);
+				changes[t] = var;
+				auto current = (nt == tau::bf_shl)
+					? bvshl_one_predicate<node>(left, var)
+					: bvrhl_one_predicate<node>(left, var);
+				predicate = predicate
+					? build_bf_and<node>(predicate, current)
+					: current;
+				break;
+			}
+			case tau::bf_mul: case tau::bf_div: case tau::bf_mod: {
+				// Unsupported operation for now
+				LOG_ERROR << "Operation "
+					<< LOG_NT(nt) << " not supported yet in predicate blasting.";
+				return error = true, false;
+			}
+			case tau::bf_nand: case tau::bf_xnor: case tau::bf_nor:{
+				// Unsupported operation for now. They must be replaced by their
+				// equivalent in terms of and, or and not for blasting.
+				LOG_ERROR << "Operation "
+					<< LOG_NT(nt) << " not supported yet in predicate blasting.";
+				return error = true, false;
+			}
+			default: {
+				break;
+			}
+		}
+		return true;
+	};
+
+	// if we have an unsupported operation, i.e. we have an unsupported operation
+	//we return nullptr to indicate failure
+	if (error) return nullptr;
+	return predicate;
+}
+
 
 template<NodeType node>
 tref eq_predicate([[maybe_unused]] tref n, [[maybe_unused]] trefs& vars) {
