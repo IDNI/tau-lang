df37d026c191166e2b4508c592990212eab19e3d
castrod <david.castroesteban@gmail.com>
2026-03-26 11:18:05 +0100

    adding helper function get_bv_type_bitwidth

--- src/boolean_algebras/bv_ba_helpers.tmpl.h
+++ src/boolean_algebras/bv_ba_helpers.tmpl.h
@@ -45,4 +45,14 @@ tref shl_by_one(tref t) {
 	return nullptr;
 }
 
+template<NodeType node>
+size_t get_bv_type_bitwidth(tref t) {
+	auto type = tree<node>::get(t).get_ba_type();
+	if (!is_bv_type_family<node>(type)) {
+		LOG_ERROR << "Type is not a bitvector type.";
+		return 0;
+	}
+	return get_bv_width<node>(type);
+}
+
 } // namespace idni::tau_lang
