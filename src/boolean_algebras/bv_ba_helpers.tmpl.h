c153e71e85ff9ad4575d406de99c0797d631ce03
castrod <david.castroesteban@gmail.com>
2026-03-31 17:17:08 +0200

    implementing bv_ba_helpers

--- src/boolean_algebras/bv_ba_helpers.tmpl.h
+++ src/boolean_algebras/bv_ba_helpers.tmpl.h
@@ -4,7 +4,7 @@
 #include "../parser/bitvector_parser.generated.h"
 
 #undef LOG_CHANNEL_NAME
-#define LOG_CHANNEL_NAME "bv_ba_hooks"
+#define LOG_CHANNEL_NAME "bv_ba_helpers"
 
 namespace idni::tau_lang {
 
@@ -12,38 +12,87 @@ using namespace cvc5;
 using namespace idni;
 
 template<NodeType node>
-bool is_bv_constant([[maybe_unused]] tref t) {
-	return false;
+bool is_bv_constant(tref t) {
+	using tau = tree<node>;
+
+	DBG( assert(tau::get(t).is_bv_constant()); )
+
+	auto type = tau::get(t).get_ba_type();
+	return is_bv_type_family<node>(type);
 }
 
 template<NodeType node>
 bool is_zero_bv_constant(tref t) {
-	DBG( assert(is_bv_constant<node>(t)); )
-	return false;
+	using tau = tree<node>;
+
+	DBG( assert(tau::get(t).is_bv_constant()); )
+
+	auto constant = tau::get(t).get_ba_constant();
+	auto bv = std::get<bv>(constant);
+	if (!bv.isBitVectorValue()) return false;
+    std::string bv_str = bv.getBitVectorValue();
+    return bv_str == "0";
 }
 
 template<NodeType node>
-tref build_bv_zero([[maybe_unused]] size_t bitwidth) {
-	// TODO (HIGH) exclude bf on the top
-	return nullptr;
+tref build_bv_zero(size_t bitwidth) {
+	using tau = tree<node>;
+
+	auto zero = make_bitvector_value(0, bitwidth);
+	auto type = bv_type_id<node>(bitwidth);
+	return tau::get_ba_constant(zero, type);
 }
 
 template<NodeType node>
-bool lsb(tref t) {
-	DBG( assert(is_bv_constant<node>(t)); )
-	return false;
+bool is_bv_lsb_one(tref t) {
+	using tau = tree<node>;
+
+	DBG( assert(tau::get(t).is_bv_constant()); )
+
+	auto constant = tau::get(t).get_ba_constant();
+	auto bv = std::get<bv>(constant);
+	if (!bv.isBitVectorValue()) return false;
+	std::string bv_str = bv.getBitVectorValue();
+	return !bv_str.empty() && bv_str.back() == '1';
 }
 
 template<NodeType node>
-tref shr_by_one(tref t) {
-	DBG( assert(is_bv_constant<node>(t)); )
-	return nullptr;
+tref bv_shr_by_one(tref t) {
+	using tau = tree<node>;
+
+	DBG( assert(tau::get(t).is_bv_constant()); )
+
+	auto type = tau::get(t).get_ba_type();
+	auto bitwidth = get_bv_width<node>(type);
+	auto constant = tau::get(t).get_ba_constant();
+	auto bv = std::get<bv>(constant);
+	if (!bv.isBitVectorValue()) return nullptr;
+	std::string bv_str = bv.getBitVectorValue();
+	if (bv_str.empty()) return nullptr;
+	bv_str.pop_back();
+	auto shifted_bv_str = bv_str.empty() ? "0" : bv_str;
+	auto shifted_bv = make_bitvector_value(bitwidth, shifted_bv_str);
+	auto new_constant = tau::get_bv_constant(shifted_bv);
+	return new_constant;
 }
 
 template<NodeType node>
-tref shl_by_one(tref t) {
-	DBG( assert(is_bv_constant<node>(t)); )
-	return nullptr;
+tref bv_shl_by_one(tref t) {
+	using tau = tree<node>;
+
+	DBG( assert(tau::get(t).is_bv_constant()); )
+
+	auto type = tau::get(t).get_ba_type();
+	auto bitwidth = get_bv_width<node>(type);
+	auto constant = tau::get(t).get_ba_constant();
+	auto bv = std::get<bv>(constant);
+	if (!bv.isBitVectorValue()) return nullptr;
+	std::string bv_str = bv.getBitVectorValue();
+	if (bv_str.empty()) return nullptr;
+	bv_str.push_back('0');
+	auto shifted_bv = make_bitvector_value(bitwidth, bv_str);
+	auto new_constant = tau::get_bv_constant(shifted_bv);
+	return new_constant;
 }
 
 template<NodeType node>
