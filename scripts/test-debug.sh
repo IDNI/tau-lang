4ed414c19af74165f74e8acf9da1b25fe5468f1f
tklip <tomas@idni.org>
2026-04-09 15:37:02 +0200

    scripts: fix argument expansion

--- scripts/test-debug.sh
+++ scripts/test-debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./dev debug -DTAU_BUILD_TESTS=ON $@ && \
+./dev debug -DTAU_BUILD_TESTS=ON "$@" && \
 	cd tests && \
 	ctest -j 8 --test-dir ../build-Debug --output-on-failure
--- scripts/test-release.sh
+++ scripts/test-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./dev release -DTAU_BUILD_TESTS=ON $@ && \
+./dev release -DTAU_BUILD_TESTS=ON "$@" && \
 	cd tests && \
 	ctest -j 8 --test-dir ../build-Release --output-on-failure
