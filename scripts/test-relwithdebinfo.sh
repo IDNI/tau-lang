189766dd9d740bee31752ba43d0d5e536e16de10
tklip <tomas@idni.org>
2026-04-09 11:25:22 +0200

    scripts: added `gdb-tau` and `test <TEST_NAME>` and `gdb <TEST_NAME>` + fixes

--- scripts/test-relwithdebinfo.sh
+++ scripts/test-relwithdebinfo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd tests
-ctest -j 8 --test-dir ../build-RelWithDebInfo --output-on-failure $@
+./dev relwithdebinfo -DTAU_BUILD_TESTS=ON $@ && \
+	cd tests && \
+	ctest -j 8 --test-dir ../build-RelWithDebInfo --output-on-failure
