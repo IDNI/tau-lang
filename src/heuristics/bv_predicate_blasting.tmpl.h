f7d342a9f121e3e6debccb1288b9ad7bb0902fe7
castrod <david.castroesteban@gmail.com>
2026-04-15 12:58:39 +0200

    improvements in code and documentation in bv_predicate_blasting_comparisons

--- src/heuristics/bv_predicate_blasting.tmpl.h
+++ src/heuristics/bv_predicate_blasting.tmpl.h
@@ -390,7 +390,7 @@ tref lt_predicate(tref atomic) {
 	if (!blasted) return nullptr;
 	auto left = tau::get(blasted).child(0);
 	auto right = tau::get(blasted).child(1);
-	auto call = make_bvlt_call<node>(left, right, bitwidth);
+	auto call = make_bvlt_call_from_index<node>(left, right, bitwidth);
 	return nso_rr_apply<node>(predicate, call);
 }
 
@@ -413,7 +413,7 @@ tref gt_predicate(tref atomic) {
 	if (!blasted) return nullptr;
 	auto left = tau::get(blasted).child(0);
 	auto right = tau::get(blasted).child(1);
-	auto call = make_bvgt_call<node>(left, right, bitwidth);
+	auto call = make_bvgt_call_from_index<node>(left, right, bitwidth);
 	return nso_rr_apply<node>(predicate, call);
 }
 
