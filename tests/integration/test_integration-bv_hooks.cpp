fd8f0665686d50eb5f6a369f9bc361c3df4b6e83
castrod <david.castroesteban@gmail.com>
2026-04-22 12:13:14 +0200

    fixing bv hooks test cases

--- tests/integration/test_integration-bv_hooks.cpp
+++ tests/integration/test_integration-bv_hooks.cpp
@@ -8,24 +8,74 @@ TEST_SUITE("configuration") {
 	TEST_CASE("bdd_init") {
 		bdd_init<Bool>();
 	}
-
-	TEST_CASE("logging") {
-		logging::trace();
-	}
 }
 
 
-// TODO (HIGH) make test cases for each operator, for example:
-// 0+1 = x1 :bv && 1+1 = x2 :bv && 1+0 = x3 :bv &&  1+{8} = x4 :bv &&  {8}+1 = x5 :bv &&  {8} + {8} = x6:bv
-// 0-1 = x1 :bv && 1-1 = x2 :bv && 1-0 = x3 :bv &&  1-{8} = x4 :bv &&  {8}-1 = x5 :bv &&  {8} - {8} = x6:bv
-// 0*1 = x1 :bv && 1*1 = x2 :bv && 1*0 = x3 :bv &&  1*{8} = x4 :bv &&  {8}*1 = x5 :bv &&  {8} * {8} = x6:bv
-// 0/1 = x1 :bv && 1/1 = x2 :bv && 1/0 = x3 :bv &&  1/{8} = x4 :bv &&  {8}/1 = x5 :bv &&  {8} / {8} = x6:bv
-// 0%1 = x1 :bv && 1%1 = x2 :bv && 1%0 = x3 :bv &&  1%{8} = x4 :bv &&  {8}%1 = x5 :bv &&  {8} % {8} = x6:bv
-// 0>>1 = x1 :bv && 1>>1 = x2 :bv && 1>>0 = x3 :bv &&  1>>{8} = x4 :bv &&  {8}>>1 = x5 :bv &&  {8} >> {8} = x6:bv
-// 0<<1 = x1 :bv && 1<<1 = x2 :bv && 1<<0 = x3 :bv &&  1<<{8} = x4 :bv &&  {8}<<1 = x5 :bv &&  {8} << {8} = x6:bv
+tref parse_bf(const std::string& sample) {
+	static tree<node_t>::get_options opts{ .parse = { .start = tree<node_t>::bf }};
+	auto src = tree<node_t>::get(sample, opts);
+	if (src == nullptr) {
+		TAU_LOG_ERROR << "Parsing failed for: " << sample;
+	}
+	return src;
+}
 
 TEST_SUITE("bv operator hooks") {
 
+	// Division by zero (should not crash, should return nullptr or error node)
+	TEST_CASE("division by zero") {
+		const char* sample = "{5}:bv[8] / {0}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("1:bv[8]");
+		CHECK(src == expected);
+	}
 
+	TEST_CASE("addition of constants") {
+		const char* sample = "{1}:bv[8] + {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("{4}:bv[8]");
+		CHECK( src == expected);
+	}
 
+	TEST_CASE("substraction of constants") {
+		const char* sample = "{1}:bv[8] - {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("{254}:bv[8]");
+		CHECK( src == expected);
+	}
+
+	TEST_CASE("multiplication of constants") {
+		const char* sample = "{3}:bv[8] * {4}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("{12}:bv[8]");
+		CHECK( src == expected);
+	}
+
+	TEST_CASE("division of constants") {
+		const char* sample = "{10}:bv[8] / {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("{3}:bv[8]");
+		CHECK( src == expected);
+	}
+
+	TEST_CASE("mod of constants") {
+		const char* sample = "{10}:bv[8] % {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("{1}:bv[8]");
+		CHECK( src == expected);
+	}
+
+	TEST_CASE("shift right of constants") {
+		const char* sample = "{16}:bv[8] >> {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("{2}:bv[8]");
+		CHECK( src == expected);
+	}
+
+	TEST_CASE("shift left of constants") {
+		const char* sample = "{1}:bv[8] << {3}:bv[8]";
+		tref src = parse_bf(sample);
+		tref expected = parse_bf("{8}:bv[8]");
+		CHECK( src == expected);
+	}
 }
