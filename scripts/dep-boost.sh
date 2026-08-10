efc62e0f35f148cf2614fe163360122f84c6f5f4
tklip <tomas@idni.org>
2026-08-10 23:52:36 +0200

    port dep-cvc5 and dep-boost onto the devrc dep_* helpers

--- scripts/dep-boost.sh
+++ scripts/dep-boost.sh
@@ -1,81 +1,39 @@
 #!/bin/bash
 
-BUILD_JOBS=4
-BOOST_REPO=https://github.com/boostorg/boost
-BOOST_TAG=65c1319   # Boost 1.86
-TAU_SHARED_PREFIX="${HOME}/.tau"
-WITH_MINGW64="no"
-BUILD_IF_NOT_EXISTS="libboost_log.a"
-TAU_BUILD_PIC="OFF"
+source "$(dirname "${BASH_SOURCE[0]}")/env"
 
-# Overwrite default values if provided in CLI arguments
-for arg in "${@:1}"; do
-	if [[ $arg == --w64 ]]; then
-		WITH_MINGW64="yes"
-		continue
-	fi
-	if [[ $arg == --darwin ]]; then
-		BUILD_IF_NOT_EXISTS="libboost_log.dylib"
-		continue
-	fi
-	if [[ $arg == -DTAU_BUILD_JOBS=* ]]; then
-		BUILD_JOBS="${arg#-DTAU_BUILD_JOBS=}"
-		continue
-	fi
-	if [[ $arg == -DTAU_SHARED_PREFIX=* ]]; then
-		TAU_SHARED_PREFIX="${arg#-DTAU_SHARED_PREFIX=}"
-		continue
-	fi
-	if [[ $arg == -DTAU_BUILD_PIC=* ]]; then
-		TAU_BUILD_PIC="${arg#-DTAU_BUILD_PIC=}"
-		continue
-	fi
-	if [[ $arg == -DBOOST_TAG=* ]]; then
-		BOOST_TAG="${arg#-DBOOST_TAG=}"
-		continue
-	fi
-done
+BOOST_REPO=https://github.com/boostorg/boost
+
+dep_entry "$@"
+
+BUILD_JOBS="$(dep_jobs)"
+TAU_SHARED_PREFIX="$(dep_shared_prefix)"
+BOOST_TAG="$(dep_var BOOST_TAG 65c1319)"   # Boost 1.86
+TAU_BUILD_PIC="$(dep_var TAU_BUILD_PIC OFF)"
+
+BUILD_IF_NOT_EXISTS="libboost_log.a"
+[[ $DEP_TARGET == darwin ]] && BUILD_IF_NOT_EXISTS="libboost_log.dylib"
 
 BOOST_SOURCE_DIR="${TAU_SHARED_PREFIX}/boost"
-if [ "$WITH_MINGW64" = "yes" ]; then
-	BOOST_BUILD_DIR=$BOOST_SOURCE_DIR/build-w64
+if [[ $DEP_TARGET == w64 ]]; then
+	BOOST_BUILD_DIR="${BOOST_SOURCE_DIR}/build-w64"
 	BOOST_PREFIX="${TAU_SHARED_PREFIX}/boost/dist-w64"
 else
-	BOOST_BUILD_DIR=$BOOST_SOURCE_DIR/build
+	BOOST_BUILD_DIR="${BOOST_SOURCE_DIR}/build"
 	BOOST_PREFIX="${TAU_SHARED_PREFIX}/boost/dist"
 fi
 
+dep_git_source "$BOOST_REPO" "$BOOST_SOURCE_DIR" "$BOOST_TAG"
 
-if [ ! -d "${BOOST_SOURCE_DIR}" ]; then
-	git clone $BOOST_REPO --branch master --single-branch ${BOOST_SOURCE_DIR}
-	cd ${BOOST_SOURCE_DIR}
-	git checkout ${BOOST_TAG}
-else
-        cd ${BOOST_SOURCE_DIR}
-	echo "boost source directory already exists, skipping clone and checkout"
-fi
-
-REBUILD="yes"
-
-if [ -f "${BOOST_PREFIX}/lib/${BUILD_IF_NOT_EXISTS}" ]; then
-	echo "${BOOST_PREFIX}/lib/${BUILD_IF_NOT_EXISTS} already exists, skipping boost building"
-	REBUILD="no"
-fi
-if [ -f "${BOOST_PREFIX}/lib64/${BUILD_IF_NOT_EXISTS}" ]; then
-	echo "${BOOST_PREFIX}/lib64/${BUILD_IF_NOT_EXISTS} already exists, skipping boost building"
-	REBUILD="no"
-fi
-
-if [ "$REBUILD" = "no" ]; then
-        exit 0
-fi
+dep_done_if_exists "${BOOST_PREFIX}/lib/${BUILD_IF_NOT_EXISTS}" "boost building"
+dep_done_if_exists "${BOOST_PREFIX}/lib64/${BUILD_IF_NOT_EXISTS}" "boost building"
 
 # initialize needed dependencies
 git submodule update --init tools/boostdep
 git submodule update --init libs/log
 python3 tools/boostdep/depinst/depinst.py log
 
-mkdir -p $BOOST_PREFIX
+mkdir -p "$BOOST_PREFIX"
 
 B2_ARGS=()
 B2_ARGS+=("address-model=64")
@@ -85,7 +43,7 @@ B2_ARGS+=("variant=release")
 # fi
 
 USER_CONFIG_ARG=""
-if [ "$WITH_MINGW64" = "yes" ]; then
+if [[ $DEP_TARGET == w64 ]]; then
 	# create user-config.jam for Windows/mingw-w64 build
 	cat > "$BOOST_SOURCE_DIR/user-config.jam" << EOF
 using gcc : mingw64 : x86_64-w64-mingw32-g++
